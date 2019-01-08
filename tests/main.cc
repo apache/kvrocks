a92f37e48c9ba9c2dfa290b2e12fa67fe5ce7834
linty <linty@meitu.com>
2019-01-08 14:06:41 +0800

    FIX glog would missing gflags::FlagRegister symbol

--- tests/main.cc
+++ tests/main.cc
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
+#include <gflags/gflags.h>
 
 int main(int argc, char **argv) {
+  gflags::SetUsageMessage("kvrocks unittest");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }

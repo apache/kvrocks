18109dad3bbd1d123f68ba33a2ade280a8d73ae9
linty <linty@meitu.com>
2019-01-09 10:30:45 +0800

    Use getopt to parse the commandline options

--- src/main.cc
+++ src/main.cc
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <event2/thread.h>
 #include <gflags/gflags.h>
 #include <glog/logging.h>
@@ -9,41 +10,68 @@
 #include "config.h"
 #include "server.h"
 
-DEFINE_string(conf_path, "../kvrocks.conf", "config file");
+const char *kDefaultConfPath = "../kvrocks.conf";
 
 std::function<void()> hup_handler;
+
+struct Options {
+  std::string conf_file = kDefaultConfPath;
+  bool show_usage = false;
+};
 
 extern "C" void signal_handler(int sig) {
   if (hup_handler) hup_handler();
 }
 
+static void usage(const char* program) {
+  std::cout << program << " implements the Redis protocol based on rocksdb\n"
+            << "\t-c config file, default is " << kDefaultConfPath << "\n"
+            << "\t-h help\n";
+  exit(0);
+}
+
+static Options *parseCommandLineOptions(int argc, char **argv) {
+  int ch;
+  auto opts = new Options();
+  while((ch = ::getopt(argc, argv, "c:h")) != -1) {
+    switch (ch) {
+      case 'c': opts->conf_file = optarg; break;
+      case 'h': opts->show_usage = true; break;
+      default: break;
+    }
+  }
+  return opts;
+}
+
 int main(int argc, char* argv[]) {
   google::InitGoogleLogging("ev");
-  gflags::SetUsageMessage("A Useless Worker");
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
   evthread_use_pthreads();
 
   signal(SIGPIPE, SIG_IGN);
   signal(SIGINT, signal_handler);
   signal(SIGTERM, signal_handler);
 
-  LOG(INFO) << "Version: " << VERSION << " @" << GIT_COMMIT;
-  Config config;
+  auto opts = parseCommandLineOptions(argc, argv);
+  if (opts->show_usage) usage(argv[0]);
+  std::string config_file_path = std::move(opts->conf_file);
+  delete opts;
+
   std::string err;
-  if (!config.Load(FLAGS_conf_path, &err)) {
+  Config config;
+  if (!config.Load(config_file_path, &err)) {
     LOG(ERROR) << "Failed to load config, err: " << err;
     exit(1);
   }
   FLAGS_logtostderr = true;
   FLAGS_minloglevel = config.loglevel;
 
+  LOG(INFO) << "Version: " << VERSION << " @" << GIT_COMMIT;
   Engine::Storage storage(&config);
   auto s = storage.Open();
   if (!s.IsOK()) {
     LOG(ERROR) << "failed to open: " << s.Msg();
     exit(1);
   }
-
   Server svr(&storage, &config);
   hup_handler = [&svr]() {
     LOG(INFO) << "bye bye";

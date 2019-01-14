7740c7e3a817982573ae0834b4ada494aba73172
liumiuyong <lmy@meitu.com>
2019-01-14 15:41:23 +0800

    Add: stats(per command calls_count and latency)

--- src/stats.h
+++ src/stats.h
@@ -5,20 +5,25 @@
 #include <string>
 #include <unistd.h>
 
+struct command_stat {
+  std::atomic<uint64_t> calls;
+  std::atomic<uint64_t> latency;
+};
+
 class Stats {
  public:
   std::atomic<uint64_t> total_calls = {0};
-  std::atomic<uint64_t> total_latency = {0};
   std::atomic<uint64_t> in_bytes = {0};
   std::atomic<uint64_t> out_bytes = {0};
 
   std::atomic<uint64_t> fullsync_counter = {0};
   std::atomic<uint64_t> psync_err_counter = {0};
   std::atomic<uint64_t> psync_ok_counter = {0};
+  std::map<std::string, command_stat> commands_stats;
 
  public:
-  void IncrCalls() { total_calls.fetch_add(1, std::memory_order_relaxed);}
-  void AddLatency(uint64_t latency) { total_latency.fetch_add(latency, std::memory_order_relaxed);}
+  void IncrCalls(const std::string &command_name);
+  void AddLatency(uint64_t latency, const std::string &command_name);
   void AddInbondBytes(uint64_t bytes) { in_bytes.fetch_add(bytes, std::memory_order_relaxed); }
   void AddOutbondBytes(uint64_t bytes) { out_bytes.fetch_add(bytes, std::memory_order_relaxed); }
   void IncrFullSyncCounter() { fullsync_counter.fetch_add(1, std::memory_order_relaxed); }

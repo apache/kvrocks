c97af932385082158cff96b9bb4ed24b9f0211a2
linty <linty@meitu.com>
2019-01-25 14:53:12 +0800

    CPPLint: fix lint warning in status.h

--- src/status.h
+++ src/status.h
@@ -27,13 +27,11 @@ class Status {
     // Network
     NetSendErr,
   };
+
   Status() : Status(cOK, "ok") {}
-  Status(Code code, std::string msg = "") : code_(code), msg_(std::move(msg)) {}
-
+  explicit Status(Code code, std::string msg = "") : code_(code), msg_(std::move(msg)) {}
   bool IsOK() { return code_ == cOK; }
-
   std::string Msg() { return msg_; }
-
   static Status OK() { return Status(cOK, "ok"); }
 
  private:

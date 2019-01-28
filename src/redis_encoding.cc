afe3d67414ea0d7a156d667dccb6edea27c2dafa
linty <linty@meitu.com>
2019-01-28 13:49:14 +0800

    Fix: cpplint warning in encoding

--- src/redis_encoding.cc
+++ src/redis_encoding.cc
@@ -5,10 +5,10 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <string.h>
-#include <string>
-
 /* Byte ordering detection */
 #include <sys/types.h> /* This will likely define BYTE_ORDER */
+
+#include <string>
 
 #ifndef BYTE_ORDER
 #if (BSD >= 199103)
@@ -22,26 +22,26 @@
 #define PDP_ENDIAN  3412    /* LSB first in word, MSW first in long (pdp)*/
 
 #if defined(__i386__) || defined(__x86_64__) || defined(__amd64__) || \
-   defined(vax) || defined(ns32000) || defined(sun386) || \
-   defined(MIPSEL) || defined(_MIPSEL) || defined(BIT_ZERO_ON_RIGHT) || \
-   defined(__alpha__) || defined(__alpha)
+  defined(vax) || defined(ns32000) || defined(sun386) || \
+  defined(MIPSEL) || defined(_MIPSEL) || defined(BIT_ZERO_ON_RIGHT) || \
+  defined(__alpha__) || defined(__alpha)
 #define BYTE_ORDER    LITTLE_ENDIAN
 #endif
 
 #if defined(__i386__) || defined(__x86_64__) || defined(__amd64__) || \
-   defined(vax) || defined(ns32000) || defined(sun386) || \
-   defined(MIPSEL) || defined(_MIPSEL) || defined(BIT_ZERO_ON_RIGHT) || \
-   defined(__alpha__) || defined(__alpha)
+  defined(vax) || defined(ns32000) || defined(sun386) || \
+  defined(MIPSEL) || defined(_MIPSEL) || defined(BIT_ZERO_ON_RIGHT) || \
+  defined(__alpha__) || defined(__alpha)
 #define BYTE_ORDER    LITTLE_ENDIAN
 #endif
 
 #if defined(sel) || defined(pyr) || defined(mc68000) || defined(sparc) || \
-    defined(is68k) || defined(tahoe) || defined(ibm032) || defined(ibm370) || \
-    defined(MIPSEB) || defined(_MIPSEB) || defined(_IBMR2) || defined(DGUX) ||\
-    defined(apollo) || defined(__convex__) || defined(_CRAY) || \
-    defined(__hppa) || defined(__hp9000) || \
-    defined(__hp9000s300) || defined(__hp9000s700) || \
-    defined (BIT_ZERO_ON_LEFT) || defined(m68k) || defined(__sparc)
+  defined(is68k) || defined(tahoe) || defined(ibm032) || defined(ibm370) || \
+  defined(MIPSEB) || defined(_MIPSEB) || defined(_IBMR2) || defined(DGUX) ||\
+  defined(apollo) || defined(__convex__) || defined(_CRAY) || \
+  defined(__hppa) || defined(__hp9000) || \
+  defined(__hp9000s300) || defined(__hp9000s700) || \
+  defined(BIT_ZERO_ON_LEFT) || defined(m68k) || defined(__sparc)
 #define BYTE_ORDER  BIG_ENDIAN
 #endif
 #endif /* linux */

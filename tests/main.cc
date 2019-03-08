9e7d432ce09338434b0681ae9b0b43511bd7911b
linty <linty@meitu.com>
2019-03-08 19:47:18 +0800

    MOD: run all unittests

--- tests/main.cc
+++ tests/main.cc
@@ -4,6 +4,5 @@
 int main(int argc, char **argv) {
   gflags::SetUsageMessage("kvrocks unittest");
   testing::InitGoogleTest(&argc, argv);
-  testing::FLAGS_gtest_filter = "RedisBitmap*";
   return RUN_ALL_TESTS();
 }

0b4e4252ad86466bb5e8a06a2a7a3ea902379e56
liumiuyong <lmy@meitu.com>
2019-03-21 16:32:11 +0800

    Add: replication test cases ci

--- tests/functional/conn.py
+++ tests/functional/conn.py
@@ -5,7 +5,10 @@ import redis
 PWD = os.path.dirname(os.path.realpath(__file__))
 WORKDIR = os.path.join(PWD,'../')
 
-def get_redis_conn():
-    r = redis.Redis("127.0.0.1", 6666, 0, "foobared")
+def get_redis_conn(master=True):
+    if master:
+        r = redis.Redis("127.0.0.1", 6666, 0, "foobared")
+    else:
+        r = redis.Redis("127.0.0.1", 6668, 0, "foobared")
     return r
 

ce70f5996c0a5b8e7c5aae48e741b623d0a77bea
linty <linty@meitu.com>
2019-04-28 17:56:54 +0800

    MOD: add write stall and delete sst event callback

--- src/event_listener.h
+++ src/event_listener.h
@@ -9,9 +9,11 @@ class KvrocksEventListener : public rocksdb::EventListener {
  public:
   explicit KvrocksEventListener(Engine::Storage *storage) : storage_(storage) {}
   ~KvrocksEventListener() override = default;
-  void OnCompactionCompleted(rocksdb::DB *db, const rocksdb::CompactionJobInfo &ci) override;
   void OnFlushCompleted(rocksdb::DB *db, const rocksdb::FlushJobInfo &ci) override;
+  void OnCompactionCompleted(rocksdb::DB *db, const rocksdb::CompactionJobInfo &ci) override;
   void OnBackgroundError(rocksdb::BackgroundErrorReason reason, rocksdb::Status *status) override;
+  void OnTableFileDeleted(const rocksdb::TableFileDeletionInfo& info) override;
+  void OnStallConditionsChanged(const rocksdb::WriteStallInfo& info) override;
  private:
   Engine::Storage *storage_ = nullptr;
 };

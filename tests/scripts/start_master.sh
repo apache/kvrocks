d2b702c9beaa48ab452300a5a112a84b5bb1fca9
linty <linty@meitu.com>
2019-05-13 14:47:33 +0800

    FIX: change dir to /data/kvrocks cause pipeline didn't work

--- tests/scripts/start_master.sh
+++ tests/scripts/start_master.sh
@@ -5,6 +5,3 @@ sed -i -e 's/daemonize no/daemonize yes/g' ./kvrocks.conf
 /cache/kvrocks -c ./kvrocks.conf
 yum install -y nc
 until nc -z 127.0.0.1 6666; do echo "kvrocks is not ready"; sleep 1; done
-
-
-
--- tests/scripts/start_slave.sh
+++ tests/scripts/start_slave.sh
@@ -3,9 +3,6 @@
 # start slave
 sed -i -e 's/port 6666/port 6668/g' ./kvrocks.conf
 sed -i -e 's/# slaveof 127.0.0.1 6379/slaveof 127.0.0.1 6666/g' ./kvrocks.conf
-sed -i -e 's/dir \/tmp\/kvrocks/dir \/tmp\/kvrocks_slave/g' ./kvrocks.conf
+sed -i -e 's/dir \/data\/kvrocks/dir \/data\/kvrocks_slave/g' ./kvrocks.conf
 /cache/kvrocks -c ./kvrocks.conf -p /var/run/kvrocks-slave.pid
 until nc -z 127.0.0.1 6668; do echo "kvrocks slave is not ready"; sleep 1; done
-
-
-

1e503c6394ba2bbc8a1224a48d4a34c0d23ad582
liumiuyong <lmy@meitu.com>
2019-06-04 14:37:04 +0800

    ADD: cmd info add rocksdb memory usage

--- src/storage.h
+++ src/storage.h
@@ -50,6 +50,7 @@ class Storage {
   rocksdb::DB *GetDB();
   const std::string GetName() {return config_->db_name; }
   rocksdb::ColumnFamilyHandle *GetCFHandle(const std::string &name);
+  std::vector<rocksdb::ColumnFamilyHandle *> GetCFHandles() { return cf_handles_; }
   LockManager *GetLockManager() { return &lock_mgr_; }
   void PurgeOldBackups(uint32_t num_backups_to_keep, uint32_t backup_max_keep_hours);
   uint64_t GetTotalSize();

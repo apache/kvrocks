6e64aa81ea1f825ff44e2aea841d4f1f8eebd576
karelrooted <karelrooted@gmail.com>
2020-04-30 10:40:40 +0800

    MOD: code style

--- src/task_runner.cc
+++ src/task_runner.cc
@@ -20,22 +20,13 @@ Status TaskRunner::Publish(Task task) {
 }
 
 void TaskRunner::Start() {
+  stop_ = false;
   for (int i = 0; i < n_thread_; i++) {
     threads_.emplace_back(std::thread([this]() {
       Util::ThreadSetName("task-runner");
       this->run();
     }));
   }
-}
-
-void TaskRunner::StopAndClear() {
-  Stop();
-  Join();
-  mu_.lock();
-  threads_.clear();
-  task_queue_.clear();
-  stop_ = false;
-  mu_.unlock();
 }
 
 void TaskRunner::Stop() {
@@ -49,6 +40,13 @@ void TaskRunner::Join() {
   for (size_t i = 0; i < threads_.size(); i++) {
     if (threads_[i].joinable()) threads_[i].join();
   }
+}
+
+void TaskRunner::Purge() {
+  mu_.lock();
+  threads_.clear();
+  task_queue_.clear();
+  mu_.unlock();
 }
 
 void TaskRunner::run() {

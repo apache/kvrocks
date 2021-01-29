89e67c56f7752d439ef6c4bb9e9687dc39541ee0
karelrooted <karelrooted@gmail.com>
2021-01-29 17:59:40 +0800

    FIX: enable cppcheck (remove performance check_type) (#159)
    * FIX: enable cppcheck (remove performance check_type)
    
    * FIX: use --force to  check all configurations (#ifdef)

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CHECK_TYPES="warning,performance,portability,information,missingInclude"
+CHECK_TYPES="warning,portability,information,missingInclude"
 STANDARD=c++11
 ERROR_EXITCODE=1
 LANG=c++
-cppcheck --enable=${CHECK_TYPES} -U__GNUC__ -x ${LANG}  src --std=${STANDARD} --error-exitcode=${ERROR_EXITCODE}
+cppcheck --force --enable=${CHECK_TYPES} -U__GNUC__ -x ${LANG}  src --std=${STANDARD} --error-exitcode=${ERROR_EXITCODE}

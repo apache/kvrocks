5bc729c54333e10a6f5f6709920517041a9c980e
karelrooted <karelrooted@gmail.com>
2021-01-29 14:42:46 +0800

    FIX: use --force to  check all configurations (#ifdef)

--- cppcheck.sh
+++ cppcheck.sh
@@ -3,4 +3,4 @@ CHECK_TYPES="warning,portability,information,missingInclude"
 STANDARD=c++11
 ERROR_EXITCODE=1
 LANG=c++
-cppcheck --enable=${CHECK_TYPES} -U__GNUC__ -x ${LANG}  src --std=${STANDARD} --error-exitcode=${ERROR_EXITCODE}
+cppcheck --force --enable=${CHECK_TYPES} -U__GNUC__ -x ${LANG}  src --std=${STANDARD} --error-exitcode=${ERROR_EXITCODE}

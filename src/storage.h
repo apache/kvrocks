8afda62651ce1a2ec2cd25748b2d7a1370142e9a
Wang Yuan <wangyuan21@baidu.com>
2021-05-06 11:37:35 +0800

    Add config purge-backup-on-fullsync (#245)
    If replicas need full synchronization with master, master need to create
    checkpoint for feeding replicas, and replicas also stage a checkpoint of
    the master. If we also keep the backup, it maybe occupy extra disk space.
    You can enable 'purge-backup-on-fullsync' if disk is not sufficient, but
    that may cause remote backup copy failing. This config is no by default.
    
    Other changes
      - Simply how to check checkpoint exists or not
      - Adjust cron for purging backup and checkpoint

--- src/storage.h
+++ src/storage.h
@@ -114,9 +114,8 @@ class Storage {
         const std::string &repl_file, uint32_t crc);
   };
 
-  bool ExistCheckpoint() { return backup_env_->FileExists(config_->checkpoint_dir).ok(); }
-  void SetCreatingCheckpoint(bool yes_or_no)  { checkpoint_info_.is_creating = yes_or_no; }
-  bool IsCreatingCheckpoint() { return checkpoint_info_.is_creating; }
+  bool ExistCheckpoint(void);
+  bool ExistSyncCheckpoint(void);
   void SetCheckpointCreateTime(time_t t)  { checkpoint_info_.create_time = t; }
   time_t GetCheckpointCreateTime()  { return checkpoint_info_.create_time; }
   void SetCheckpointAccessTime(time_t t)  { checkpoint_info_.access_time = t; }

0d5bba14d06550a391774440360c11101a27b11c
hulk <hulk.website@gmail.com>
2021-05-06 21:20:26 +0800

    Refactor the redis command table to support attributes (#248)

--- src/main.cc
+++ src/main.cc
@@ -299,6 +299,7 @@ int main(int argc, char* argv[]) {
   }
 
   LOG(INFO) << "Version: " << VERSION << " @" << GIT_COMMIT;
+  Redis::PopulateCommands();
   Engine::Storage storage(&config);
   s = storage.Open();
   if (!s.IsOK()) {

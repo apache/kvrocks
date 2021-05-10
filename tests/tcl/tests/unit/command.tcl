755483265f3dcfa69b6a87999e3dde62b502d1b3
liumiuyong <lmy@meitu.com>
2021-05-10 14:10:53 +0800

    Add testcase for COMMAND command

---
+++ tests/tcl/tests/unit/command.tcl
@@ -0,0 +1,33 @@
+start_server {tags {"command"}} {
+    test {COMMAND count} {
+        r command count
+    } {155}
+
+    test {COMMAND info} {
+        set e [lindex [r command info get mget] 0]
+        assert_equal [llength $e] 6
+        assert_equal [lindex $e 0] get
+        assert_equal [lindex $e 1] 2
+        assert_equal [lindex $e 2] {readonly}
+        assert_equal [lindex $e 3] 1
+        assert_equal [lindex $e 4] 1
+        assert_equal [lindex $e 5] 1
+    }
+
+    test {COMMAND} {
+        set e [lindex [r command] 16]
+        assert_equal [llength $e] 6
+        assert_equal [lindex $e 0] decr
+        assert_equal [lindex $e 1] 2
+        assert_equal [lindex $e 2] {write}
+        assert_equal [lindex $e 3] 1
+        assert_equal [lindex $e 4] 1
+        assert_equal [lindex $e 5] 1
+    }
+
+    test {COMMAND getkeys} {
+        assert_equal {test} [r command getkeys get test]
+        assert_equal {test test2} [r command getkeys mget test test2]
+        assert_equal {test} [r command getkeys zadd test 1 m1]
+    }
+}

deaa37d92d77133c5161f1a80642d97a56942bd0
liumiuyong <lmy@meitu.com>
2021-05-10 16:56:56 +0800

    Apply suggestions from code review

--- tests/tcl/tests/unit/command.tcl
+++ tests/tcl/tests/unit/command.tcl
@@ -1,10 +1,10 @@
 start_server {tags {"command"}} {
-    test {the total commands count is 155} {
+    test {kvrocks has 155 commands currently} {
         r command count
     } {155}
 
-    test {COMMAND info} {
-        set e [lindex [r command info get mget] 0]
+    test {acquire GET command info by COMMAND INFO} {
+        set e [lindex [r command info get] 0]
         assert_equal [llength $e] 6
         assert_equal [lindex $e 0] get
         assert_equal [lindex $e 1] 2
@@ -19,7 +19,7 @@ start_server {tags {"command"}} {
         assert_equal [llength $e] 6
     }
 
-    test {COMMAND getkeys} {
+    test {get keys of commands by COMMAND GETKEYS} {
         assert_equal {test} [r command getkeys get test]
         assert_equal {test test2} [r command getkeys mget test test2]
         assert_equal {test} [r command getkeys zadd test 1 m1]

b47f16544e78ec11d63bc1a3402a2a3778e5d333
Wang Yuan <wangyuan21@baidu.com>
2021-06-16 11:15:58 +0800

    Transfer kvrocks from bitleak to kvrockslabs (#308)

--- package.sh
+++ package.sh
@@ -5,6 +5,6 @@ STAGE=${STAGE:-release}
 fpm -f -s dir -t rpm --prefix '/www/kvrocks'  -n ${NAME} --epoch 7 \
     --config-files /www/kvrocks/conf/kvrocks.conf \
     -v ${VERSION} --iteration ${CI_PIPELINE_ID}.${STAGE} -C ./_build \
-    --verbose --category 'bitleak/projects' --description 'kvrocks' \
-    --url 'https://github.com/bitleak' --license 'BSD'
+    --verbose --category 'kvrockslabs/projects' --description 'kvrocks' \
+    --url 'https://github.com/kvrockslabs' --license 'BSD'
 

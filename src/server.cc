7a5e50ac3605551987bda882f7948ea2376ba776
Wang Yuan <wangyuan21@baidu.com>
2021-09-10 17:10:57 +0800

    Fix wrong mater port if enabling master-use-repl-port (#360)
    If enabling master-use-repl-port, in replica, its master port is wrong,
    it should be master listen port instead of listen port + 1.

--- src/server.cc
+++ src/server.cc
@@ -158,9 +158,10 @@ Status Server::AddMaster(std::string host, uint32_t port, bool force_reconnect) 
 
   // For master using old version, it uses replication thread to implement
   // replication, and uses 'listen-port + 1' as thread listening port.
-  if (GetConfig()->master_use_repl_port) port += 1;
+  uint32_t master_listen_port = port;
+  if (GetConfig()->master_use_repl_port)  master_listen_port += 1;
   replication_thread_ = std::unique_ptr<ReplicationThread>(
-      new ReplicationThread(host, port, this, config_->masterauth));
+      new ReplicationThread(host, master_listen_port, this, config_->masterauth));
   auto s = replication_thread_->Start(
       [this]() {
         PrepareRestoreDB();

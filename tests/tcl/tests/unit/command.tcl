65756d411dc1aeee1bf8a704b5f705056dfdd503
nio <nioshield@gmail.com>
2021-11-03 22:32:18 +0800

    Implement zrevrangebylex command (#381)

--- tests/tcl/tests/unit/command.tcl
+++ tests/tcl/tests/unit/command.tcl
@@ -1,7 +1,7 @@
 start_server {tags {"command"}} {
-    test {kvrocks has 163 commands currently} {
+    test {kvrocks has 164 commands currently} {
         r command count
-    } {163}
+    } {164}
 
     test {acquire GET command info by COMMAND INFO} {
         set e [lindex [r command info get] 0]

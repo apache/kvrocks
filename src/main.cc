a524aadb8e1c1c775987ebe0879025b2632bb960
wangyuan21 <wangyuan21@baidu.com>
2022-01-19 11:37:17 +0800

    Reset macOS changes

--- src/main.cc
+++ src/main.cc
@@ -63,7 +63,7 @@ void *getMcontextEip(ucontext_t *uc) {
 #if defined(_STRUCT_X86_THREAD_STATE64) && !defined(__i386__)
         return reinterpret_cast<void*>(uc->uc_mcontext->__ss.__rip);
 #else
-        return reinterpret_cast<void*>(uc->uc_mcontext->__ss.__pc);
+        return reinterpret_cast<void*>(uc->uc_mcontext->__ss.__eip);
 #endif
 #elif defined(__i386__) || defined(__X86_64__) || defined(__x86_64__)
         return reinterpret_cast<void*>(uc->uc_mcontext.gregs[REG_EIP]); /* Linux 32/64 bit */

424e1784b00b2798c60da0bfdbfee8a4291ecfcc
bitstring <i@eventloop.live>
2022-03-07 11:41:01 +0800

    Fix blob file size unit error and set default value to 256 MB (#503)

--- tests/config_test.cc
+++ tests/config_test.cc
@@ -51,7 +51,7 @@ TEST(Config, GetAndSet) {
       {"rocksdb.level0_stop_writes_trigger", "100"},
       {"rocksdb.enable_blob_files", "no"},
       {"rocksdb.min_blob_size", "4096"},
-      {"rocksdb.blob_file_size", "128"},
+      {"rocksdb.blob_file_size", "256"},
       {"rocksdb.enable_blob_garbage_collection", "yes"},
       {"rocksdb.blob_garbage_collection_age_cutoff", "25"},
       {"rocksdb.max_bytes_for_level_base", "256"},

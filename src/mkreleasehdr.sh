e154a3f1879e7be8e18d8d478b26775392603ada
tison <wander4096@gmail.com>
2022-04-29 11:13:30 +0800

    Apply Apache License header for source code  (#543)
    Signed-off-by: tison <wander4096@gmail.com>

--- src/mkreleasehdr.sh
+++ src/mkreleasehdr.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 GIT_SHA1=`(git show-ref --head --hash=8 2> /dev/null || echo 00000000) | head -n1`
 GIT_DIRTY=`git diff --no-ext-diff 2> /dev/null | wc -l`
 VERSION=`grep -i "version" -m1 ../Changelog|awk '{printf $3}'`
--- src/rw_lock.h
+++ src/rw_lock.h
@@ -1,7 +1,26 @@
-// Copyright Bing-a-ling (https://github.com/Bing-a-ling/ReadWriteLock)
-// Copyright Kvrocks Core Team (bugfix and adapted to kvrocks coding style)
-// All rights reserved.
-//
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+// This file is modified from Bing-a-ling/ReadWriteLock.
+// See the original code at https://github.com/Bing-a-ling/ReadWriteLock.
+
 // A implementation of RAII write-first read-write lock using C++11.
 
 #pragma once

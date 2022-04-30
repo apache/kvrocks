5e00edfd685507fa3108570862f3a5fd4982bfe1
tison <wander4096@gmail.com>
2022-04-30 15:20:45 +0800

    Apply Apache License header for Tcl files (#548)
    Signed-off-by: tison <wander4096@gmail.com>

--- tests/tcl/tests/instances.tcl
+++ tests/tcl/tests/instances.tcl
@@ -1,11 +1,30 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# Copyright (c) 2014, Salvatore Sanfilippo
+# See bundled license file licenses/LICENSE.redis for details.
+
+# This file is copied and modified from the Redis project,
+# which started out as: https://github.com/redis/redis/blob/dbcc0a8/tests/instances.tcl
+
 # Multi-instance test framework.
 # This is used in order to test Sentinel and Redis Cluster, and provides
 # basic capabilities for spawning and handling N parallel Redis / Sentinel
 # instances.
-#
-# Copyright (C) 2014 Salvatore Sanfilippo antirez@gmail.com
-# This software is released under the BSD License. See the COPYING file for
-# more information.
 
 package require Tcl 8.5
 

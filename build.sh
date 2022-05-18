fa7dbc33cdaae8cef3f282d94e42fd4c010bd163
Twice <i@twice.moe>
2022-05-18 16:24:50 +0800

    Refactor CMakeLists: introduce target kvrocks_objs to avoid redundance (#586)
    - file(GLOB ...) is used to avoid handwritten source file lists.
    - Do not need to modify CMakeLists.txt while you add/remove some source files.
    - Any common properties e.g. compile options, include directories etc. can be
      added only to kvrocks_objs target, not every target including kvrocks,
      kvrocks2redis and unittest.
    
    Other changes
    - Fix ENBALE_ASAN typo.
    - Use RelWithDebInfo mode for cmake in build.sh

--- build.sh
+++ build.sh
@@ -64,4 +64,4 @@ if [ "$(printf '%s\n' "$CMAKE_REQUIRE_VERSION" "$CMAKE_VERSION" | sort -V | head
     CMAKE_BIN=$CMAKE_INSTALL_DIR/bin/cmake
 fi
 
-cd $BUILD_DIR && $CMAKE_BIN -DCMAKE_BUILD_TYPE=Release .. && make -j4 kvrocks kvrocks2redis
+cd $BUILD_DIR && $CMAKE_BIN -DCMAKE_BUILD_TYPE=RelWithDebInfo .. && make -j4 kvrocks kvrocks2redis

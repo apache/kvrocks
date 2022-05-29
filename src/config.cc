90643536c767282525c6d6ff03ae0486726d9343
bitstring <wellschuan@gmail.com>
2022-05-30 00:57:05 +0800

    Update config.cc

--- src/config.cc
+++ src/config.cc
@@ -458,11 +458,17 @@ void Config::initFieldCallback() {
       {"rocksdb.max_bytes_for_level_multiplier", [this](Server* srv, const std::string &k,
                                                    const std::string& v)->Status {
         if (!srv) return Status::OK();
+        if (!RocksDB.level_compaction_dynamic_level_bytes) {
+          return Status(Status::NotOK, errNotSetLevelCompactionDynamicLevelBytes);
+        }                  
         return srv->storage_->SetColumnFamilyOption(trimRocksDBPrefix(k), v);
       }},
       {"rocksdb.level_compaction_dynamic_level_bytes", [this](Server* srv, const std::string &k,
                                                         const std::string& v)->Status {
         if (!srv) return Status::OK();
+        if (!RocksDB.level_compaction_dynamic_level_bytes) {
+          return Status(Status::NotOK, errNotSetLevelCompactionDynamicLevelBytes);
+        }                  
         std::string level_compaction_dynamic_level_bytes = v == "yes" ? "true" : "false";
         return srv->storage_->SetDBOption(trimRocksDBPrefix(k), level_compaction_dynamic_level_bytes);
       }},

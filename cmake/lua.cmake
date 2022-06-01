3ddb60b7fc65f5c4a1485d291c510e846be13cc5
PragmaTwice <i@twice.moe>
2022-06-01 09:38:24 +0800

    add workaround

--- cmake/lua.cmake
+++ cmake/lua.cmake
@@ -28,9 +28,19 @@ FetchContent_GetProperties(lua)
 if(NOT lua_POPULATED)
   FetchContent_Populate(lua)
 
+  set(LUA_CXX ${CMAKE_CXX_COMPILER})
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+    try_compile(FOUND_ASSERT_H ${PROJECT_BINARY_DIR} ${PROJECT_SOURCE_DIR}/cmake/checks/include_assert_h.c)
+
+    if(NOT FOUND_ASSERT_H)
+      message(STATUS "fail to compile cmake/checks/include_assert_h.c, use ${CXX} to compile lua instead of ${CMAKE_CXX_COMPILER}")
+      set(LUA_CXX ${CXX})
+    endif()
+  endif()
+
   set(LUA_CFLAGS "${CMAKE_CXX_FLAGS} -fpermissive -DLUA_ANSI -DENABLE_CJSON_GLOBAL -DREDIS_STATIC= -DLUA_USE_MKSTEMP")
 
-  add_custom_target(make_lua COMMAND make "CC=${CMAKE_CXX_COMPILER}" "CFLAGS=${LUA_CFLAGS}" liblua.a
+  add_custom_target(make_lua COMMAND make "CC=${LUA_CXX}" "CFLAGS=${CMAKE_CXX_FLAGS}" liblua.a
     WORKING_DIRECTORY ${lua_SOURCE_DIR}/src
   )
   

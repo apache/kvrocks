420ad978d34a72b516e3b873429c6b1c68f0bdb2
PragmaTwice <twice.mliu@gmail.com>
2022-06-21 23:43:20 +0800

    fix

--- src/event_utils.h
+++ src/event_utils.h
@@ -42,8 +42,8 @@ struct UniqueFreePtr : std::unique_ptr<T, StaticFree> {
 };
 
 struct UniqueEvbufReadln : UniqueFreePtr<char[]> {
-    UniqueEvbufReadln(evbuffer* buffer, evbuffer_eol_style eol_style) // cppcheck-suppress uninitMemberVar
-      : UniqueFreePtr(evbuffer_readln(buffer, &length, eol_style)) {}  
+    UniqueEvbufReadln(evbuffer* buffer, evbuffer_eol_style eol_style)  // cppcheck-suppress uninitMemberVar
+      : UniqueFreePtr(evbuffer_readln(buffer, &length, eol_style)) {}
 
     size_t length;
 };

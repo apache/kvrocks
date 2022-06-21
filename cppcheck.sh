85b5d77fc9987637fe64a711d91fe0d88a3546d2
PragmaTwice <twice.mliu@gmail.com>
2022-06-21 22:55:04 +0800

    add --suppress=missingIncludeSystem to cppcheck

--- cppcheck.sh
+++ cppcheck.sh
@@ -21,4 +21,4 @@ CHECK_TYPES="warning,portability,information,missingInclude"
 STANDARD=c++11
 ERROR_EXITCODE=1
 LANG=c++
-cppcheck --force --enable=${CHECK_TYPES} -U__GNUC__ -x ${LANG}  src --std=${STANDARD} --error-exitcode=${ERROR_EXITCODE}
+cppcheck --force --enable=${CHECK_TYPES} -U__GNUC__ -x ${LANG}  src --std=${STANDARD} --error-exitcode=${ERROR_EXITCODE} --suppress=missingIncludeSystem

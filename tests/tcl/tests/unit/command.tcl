1bcb7dbe39af5ad16a39743420976a841c38fa14
mathspanda <mathspanda826@gmail.com>
2022-08-16 09:51:46 +0800

    Add the new command SMISMEMBER (#778)

--- tests/tcl/tests/unit/command.tcl
+++ tests/tcl/tests/unit/command.tcl
@@ -16,9 +16,9 @@
 # under the License.
 
 start_server {tags {"command"}} {
-    test {kvrocks has 171 commands currently} {
+    test {kvrocks has 172 commands currently} {
         r command count
-    } {171}
+    } {172}
 
     test {acquire GET command info by COMMAND INFO} {
         set e [lindex [r command info get] 0]

2e291762f15757ae8441d7c6c0d3a08282c2ac49
hulk <hulk.website@gmail.com>
2022-08-19 10:14:56 +0800

    Update src/util.h

--- src/util.h
+++ src/util.h
@@ -74,6 +74,7 @@ template <typename T, typename... Args>
 std::unique_ptr<T> MakeUnique(Args&& ... args) {
   return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
+
 template <typename T, typename... Args>
 std::shared_ptr<T> MakeShared(Args&& ... args) {
   return std::shared_ptr<T>(new T(std::forward<Args>(args)...));

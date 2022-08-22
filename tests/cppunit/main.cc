4321164486805ed58d29e9e6f1b7afa77cec34ad
PragmaTwice <twice.mliu@gmail.com>
2022-08-22 13:37:35 +0800

    fix gtest main

--- tests/cppunit/main.cc
+++ tests/cppunit/main.cc
@@ -28,6 +28,7 @@ Server *GetServer() {
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
   return RUN_ALL_TESTS();
 }

a51d3436d0bb62bdb8c022711c169f153e955413
git-hulk <hulk.website@gmail.com>
2022-09-11 10:49:49 +0800

    Use require to replace assert

--- tests/gocase/unit/keyspace_test.go
+++ tests/gocase/unit/keyspace_test.go
@@ -25,8 +25,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/apache/incubator-kvrocks/tests/gocase/util"
 	"github.com/stretchr/testify/require"
 )
@@ -42,29 +40,29 @@ func TestKeyspace(t *testing.T) {
 	t.Run("DEL against a single item", func(t *testing.T) {
 		key := "x"
 		value := "foo"
-		assert.Nil(t, rdb.Set(ctx, key, value, 0).Err())
-		assert.Equal(t, value, rdb.Get(ctx, key).Val())
-		assert.Equal(t, int64(1), rdb.Del(ctx, key).Val())
-		assert.Equal(t, "", rdb.Get(ctx, key).Val())
+		require.NoError(t, rdb.Set(ctx, key, value, 0).Err())
+		require.Equal(t, value, rdb.Get(ctx, key).Val())
+		require.Equal(t, int64(1), rdb.Del(ctx, key).Val())
+		require.Equal(t, "", rdb.Get(ctx, key).Val())
 	})
 
 	t.Run("Vararg DEL", func(t *testing.T) {
-		assert.Nil(t, rdb.Set(ctx, "foo1", "a", 0).Err())
-		assert.Nil(t, rdb.Set(ctx, "foo2", "b", 0).Err())
-		assert.Nil(t, rdb.Set(ctx, "foo3", "c", 0).Err())
-		assert.Equal(t, int64(3), rdb.Del(ctx, "foo1", "foo2", "foo3").Val())
-		assert.Equal(t, []interface{}{nil, nil, nil}, rdb.MGet(ctx, "foo1", "foo2", "foo3").Val())
+		require.NoError(t, rdb.Set(ctx, "foo1", "a", 0).Err())
+		require.NoError(t, rdb.Set(ctx, "foo2", "b", 0).Err())
+		require.NoError(t, rdb.Set(ctx, "foo3", "c", 0).Err())
+		require.Equal(t, int64(3), rdb.Del(ctx, "foo1", "foo2", "foo3").Val())
+		require.Equal(t, []interface{}{nil, nil, nil}, rdb.MGet(ctx, "foo1", "foo2", "foo3").Val())
 	})
 
 	t.Run("KEYS with pattern", func(t *testing.T) {
 		for _, key := range []string{"key_x", "key_y", "key_z", "foo_a", "foo_b", "foo_c"} {
-			assert.Nil(t, rdb.Set(ctx, key, "hello", 0).Err())
+			require.NoError(t, rdb.Set(ctx, key, "hello", 0).Err())
 		}
 		keys := rdb.Keys(ctx, "foo*").Val()
 		sort.Slice(keys, func(i, j int) bool {
 			return keys[i] < keys[j]
 		})
-		assert.Equal(t, []string{"foo_a", "foo_b", "foo_c"}, keys)
+		require.Equal(t, []string{"foo_a", "foo_b", "foo_c"}, keys)
 	})
 
 	t.Run("KEYS to get all keys", func(t *testing.T) {
@@ -72,31 +70,31 @@ func TestKeyspace(t *testing.T) {
 		sort.Slice(keys, func(i, j int) bool {
 			return keys[i] < keys[j]
 		})
-		assert.Equal(t, []string{"foo_a", "foo_b", "foo_c", "key_x", "key_y", "key_z"}, keys)
+		require.Equal(t, []string{"foo_a", "foo_b", "foo_c", "key_x", "key_y", "key_z"}, keys)
 	})
 
 	t.Run("DEL all keys", func(t *testing.T) {
 		vals := rdb.Keys(ctx, "*").Val()
-		assert.Equal(t, int64(len(vals)), rdb.Del(ctx, vals...).Val())
-		assert.Nil(t, rdb.Do(ctx, "dbsize", "scan").Err())
+		require.Equal(t, int64(len(vals)), rdb.Del(ctx, vals...).Val())
+		require.NoError(t, rdb.Do(ctx, "dbsize", "scan").Err())
 		time.Sleep(100 * time.Millisecond)
-		assert.Equal(t, int64(0), rdb.Do(ctx, "dbsize").Val())
+		require.Equal(t, int64(0), rdb.Do(ctx, "dbsize").Val())
 	})
 
 	t.Run("EXISTS", func(t *testing.T) {
 		newKey := "newkey"
-		assert.Nil(t, rdb.Set(ctx, newKey, "test", 0).Err())
-		assert.Equal(t, int64(1), rdb.Exists(ctx, newKey).Val())
-		assert.Equal(t, int64(1), rdb.Del(ctx, newKey).Val())
-		assert.Equal(t, int64(0), rdb.Exists(ctx, newKey).Val())
+		require.NoError(t, rdb.Set(ctx, newKey, "test", 0).Err())
+		require.Equal(t, int64(1), rdb.Exists(ctx, newKey).Val())
+		require.Equal(t, int64(1), rdb.Del(ctx, newKey).Val())
+		require.Equal(t, int64(0), rdb.Exists(ctx, newKey).Val())
 	})
 
 	t.Run("Zero length value in key. SET/GET/EXISTS", func(t *testing.T) {
 		emptyKey := "emptykey"
-		assert.Nil(t, rdb.Set(ctx, emptyKey, nil, 0).Err())
-		assert.Equal(t, int64(1), rdb.Exists(ctx, emptyKey).Val())
-		assert.Equal(t, int64(1), rdb.Del(ctx, emptyKey).Val())
-		assert.Equal(t, int64(0), rdb.Exists(ctx, emptyKey).Val())
+		require.NoError(t, rdb.Set(ctx, emptyKey, nil, 0).Err())
+		require.Equal(t, int64(1), rdb.Exists(ctx, emptyKey).Val())
+		require.Equal(t, int64(1), rdb.Del(ctx, emptyKey).Val())
+		require.Equal(t, int64(0), rdb.Exists(ctx, emptyKey).Val())
 	})
 
 	t.Run("Commands pipelining", func(t *testing.T) {
@@ -105,26 +103,26 @@ func TestKeyspace(t *testing.T) {
 		require.NoError(t, c.Write("SET k1 xyzk\r\nGET k1\r\nPING\r\n"))
 		r, err := c.ReadLine()
 		require.NoError(t, err)
-		assert.Equal(t, "+OK", r)
+		require.Equal(t, "+OK", r)
 		r, err = c.ReadLine()
 		require.NoError(t, err)
-		assert.Equal(t, "$4", r)
+		require.Equal(t, "$4", r)
 		r, err = c.ReadLine()
 		require.NoError(t, err)
-		assert.Equal(t, "xyzk", r)
+		require.Equal(t, "xyzk", r)
 		r, err = c.ReadLine()
 		require.NoError(t, err)
-		assert.Equal(t, "+PONG", r)
+		require.Equal(t, "+PONG", r)
 	})
 
 	t.Run("Non existing command", func(t *testing.T) {
-		assert.Contains(t, rdb.Do(ctx, "foobaredcommand").Err().Error(), "ERR")
+		require.Contains(t, rdb.Do(ctx, "foobaredcommand").Err().Error(), "ERR")
 	})
 
 	t.Run("RANDOMKEY", func(t *testing.T) {
 		rdb.FlushDB(ctx)
-		assert.Nil(t, rdb.Set(ctx, "foo", "x", 0).Err())
-		assert.Nil(t, rdb.Set(ctx, "bar", "y", 0).Err())
+		require.NoError(t, rdb.Set(ctx, "foo", "x", 0).Err())
+		require.NoError(t, rdb.Set(ctx, "bar", "y", 0).Err())
 
 		fooSeen := 0
 		barSeen := 0
@@ -140,25 +138,25 @@ func TestKeyspace(t *testing.T) {
 			}
 			time.Sleep(10 * time.Millisecond)
 		}
-		assert.Greater(t, fooSeen+barSeen, 0)
+		require.Greater(t, fooSeen+barSeen, 0)
 	})
 
 	t.Run("RANDOMKEY against empty DB", func(t *testing.T) {
 		rdb.FlushDB(ctx)
-		assert.Equal(t, "", rdb.RandomKey(ctx).Val())
+		require.Equal(t, "", rdb.RandomKey(ctx).Val())
 	})
 
 	t.Run("RANDOMKEY regression 1", func(t *testing.T) {
 		rdb.FlushDB(ctx)
-		assert.Nil(t, rdb.Set(ctx, "x", 10, 0).Err())
-		assert.Equal(t, int64(1), rdb.Del(ctx, "x").Val())
-		assert.Equal(t, "", rdb.RandomKey(ctx).Val())
+		require.NoError(t, rdb.Set(ctx, "x", 10, 0).Err())
+		require.Equal(t, int64(1), rdb.Del(ctx, "x").Val())
+		require.Equal(t, "", rdb.RandomKey(ctx).Val())
 	})
 
 	t.Run("KEYS * two times with long key, Github issue #1208", func(t *testing.T) {
 		rdb.FlushDB(ctx)
-		assert.Nil(t, rdb.Set(ctx, "dlskeriewrioeuwqoirueioqwrueoqwrueqw", "teset", 0).Err())
-		assert.Equal(t, []string{"dlskeriewrioeuwqoirueioqwrueoqwrueqw"}, rdb.Keys(ctx, "*").Val())
+		require.NoError(t, rdb.Set(ctx, "dlskeriewrioeuwqoirueioqwrueoqwrueqw", "teset", 0).Err())
+		require.Equal(t, []string{"dlskeriewrioeuwqoirueioqwrueoqwrueqw"}, rdb.Keys(ctx, "*").Val())
 	})
 
 	t.Run("KEYS with multi namespace", func(t *testing.T) {
@@ -184,7 +182,7 @@ func TestKeyspace(t *testing.T) {
 		for token, keys := range tokenKeys {
 			rdb.Do(ctx, "auth", token)
 			for _, key := range keys {
-				assert.Nil(t, rdb.Set(ctx, key, "hello", 0).Err())
+				require.NoError(t, rdb.Set(ctx, key, "hello", 0).Err())
 			}
 		}
 		for token, keys := range tokenKeys {
@@ -193,7 +191,7 @@ func TestKeyspace(t *testing.T) {
 			sort.Slice(gotKeys, func(i, j int) bool {
 				return gotKeys[i] < gotKeys[j]
 			})
-			assert.Equal(t, keys, gotKeys)
+			require.Equal(t, keys, gotKeys)
 		}
 
 		for token, prefixKeys := range tokenPrefixKeys {
@@ -202,7 +200,7 @@ func TestKeyspace(t *testing.T) {
 			sort.Slice(gotKeys, func(i, j int) bool {
 				return gotKeys[i] < gotKeys[j]
 			})
-			assert.Equal(t, prefixKeys, gotKeys)
+			require.Equal(t, prefixKeys, gotKeys)
 		}
 	})
 }

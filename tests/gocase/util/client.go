a8838aca6d408c98d4d0cdf932855944e72f9bf8
tison <wander4096@gmail.com>
2022-09-30 19:08:13 +0800

    Move TCL test unit/scan to Go case  (#934)
    Signed-off-by: tison <wander4096@gmail.com>

--- tests/gocase/util/client.go
+++ tests/gocase/util/client.go
@@ -31,25 +31,37 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func FindInfoEntry(t *testing.T, ctx context.Context, rdb *redis.Client, key string, section ...string) string {
-	r := rdb.Info(ctx, section...)
+func FindInfoEntry(t testing.TB, rdb *redis.Client, key string, section ...string) string {
+	r := rdb.Info(context.Background(), section...)
 	p := regexp.MustCompile(fmt.Sprintf("%s:(.+)", key))
 	ms := p.FindStringSubmatch(r.Val())
 	require.Len(t, ms, 2)
 	return strings.TrimSpace(ms[1])
 }
 
-func WaitForSync(t *testing.T, ctx context.Context, slave *redis.Client) {
+func WaitForSync(t testing.TB, slave *redis.Client) {
 	require.Eventually(t, func() bool {
-		r := FindInfoEntry(t, ctx, slave, "master_link_status")
+		r := FindInfoEntry(t, slave, "master_link_status")
 		return r == "up"
 	}, 5*time.Second, 100*time.Millisecond)
 }
 
-func WaitForOffsetSync(t *testing.T, ctx context.Context, master, slave *redis.Client) {
+func WaitForOffsetSync(t testing.TB, master, slave *redis.Client) {
 	require.Eventually(t, func() bool {
-		o1 := FindInfoEntry(t, ctx, master, "master_repl_offset")
-		o2 := FindInfoEntry(t, ctx, slave, "master_repl_offset")
+		o1 := FindInfoEntry(t, master, "master_repl_offset")
+		o2 := FindInfoEntry(t, slave, "master_repl_offset")
 		return o1 == o2
 	}, 5*time.Second, 100*time.Millisecond)
+}
+
+func Populate(t testing.TB, rdb *redis.Client, prefix string, n, size int) {
+	ctx := context.Background()
+	p := rdb.Pipeline()
+
+	for i := 0; i < n; i++ {
+		p.Do(ctx, "SET", fmt.Sprintf("%s%d", prefix, i), strings.Repeat("A", size))
+	}
+
+	_, err := p.Exec(ctx)
+	require.NoError(t, err)
 }

c0c8cc854dfd83cce8d47195ca1a82c91629052b
PragmaTwice <twice.mliu@gmail.com>
2022-10-19 18:31:27 +0800

    modify action config

--- tests/gocase/tls/tls_test.go
+++ tests/gocase/tls/tls_test.go
@@ -22,6 +22,7 @@ package tls
 import (
 	"context"
 	"crypto/tls"
+	"flag"
 	"testing"
 
 	"github.com/apache/incubator-kvrocks/tests/gocase/util"
@@ -29,39 +30,43 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var enableTlsTests = flag.Bool("enableTlsTests", false, "enable TLS-related test cases")
+
 func TestTLS(t *testing.T) {
-	srv := util.StartTLSServer(t, map[string]string{})
-	defer srv.Close()
+	if *enableTlsTests {
+		srv := util.StartTLSServer(t, map[string]string{})
+		defer srv.Close()
 
-	ctx := context.Background()
+		ctx := context.Background()
 
-	rdb := srv.NewTLSClient()
-	defer func() { require.NoError(t, rdb.Close()) }()
+		rdb := srv.NewTLSClient()
+		defer func() { require.NoError(t, rdb.Close()) }()
 
-	t.Run("TLS: Not accepting non-TLS connections on a TLS port", func(t *testing.T) {
-		c := srv.NewTLSClientWithOption(&redis.Options{})
-		defer func() { require.NoError(t, c.Close()) }()
-		require.Error(t, c.Ping(ctx).Err())
-	})
+		t.Run("TLS: Not accepting non-TLS connections on a TLS port", func(t *testing.T) {
+			c := srv.NewTLSClientWithOption(&redis.Options{})
+			defer func() { require.NoError(t, c.Close()) }()
+			require.Error(t, c.Ping(ctx).Err())
+		})
 
-	t.Run("TLS: Verify tls-auth-clients behaves as expected", func(t *testing.T) {
-		c := srv.NewTLSClientWithOption(&redis.Options{TLSConfig: &tls.Config{InsecureSkipVerify: true}})
-		defer func() { require.NoError(t, c.Close()) }()
-		require.Error(t, c.Ping(ctx).Err())
+		t.Run("TLS: Verify tls-auth-clients behaves as expected", func(t *testing.T) {
+			c := srv.NewTLSClientWithOption(&redis.Options{TLSConfig: &tls.Config{InsecureSkipVerify: true}})
+			defer func() { require.NoError(t, c.Close()) }()
+			require.Error(t, c.Ping(ctx).Err())
 
-		require.NoError(t, rdb.ConfigSet(ctx, "tls-auth-clients", "no").Err())
-		require.NoError(t, c.Close())
-		c = srv.NewTLSClientWithOption(&redis.Options{TLSConfig: &tls.Config{InsecureSkipVerify: true}})
-		require.Equal(t, "PONG", c.Ping(ctx).Val())
+			require.NoError(t, rdb.ConfigSet(ctx, "tls-auth-clients", "no").Err())
+			require.NoError(t, c.Close())
+			c = srv.NewTLSClientWithOption(&redis.Options{TLSConfig: &tls.Config{InsecureSkipVerify: true}})
+			require.Equal(t, "PONG", c.Ping(ctx).Val())
 
-		require.NoError(t, rdb.ConfigSet(ctx, "tls-auth-clients", "optional").Err())
-		require.NoError(t, c.Close())
-		c = srv.NewTLSClientWithOption(&redis.Options{TLSConfig: &tls.Config{InsecureSkipVerify: true}})
-		require.Equal(t, "PONG", c.Ping(ctx).Val())
+			require.NoError(t, rdb.ConfigSet(ctx, "tls-auth-clients", "optional").Err())
+			require.NoError(t, c.Close())
+			c = srv.NewTLSClientWithOption(&redis.Options{TLSConfig: &tls.Config{InsecureSkipVerify: true}})
+			require.Equal(t, "PONG", c.Ping(ctx).Val())
 
-		require.NoError(t, rdb.ConfigSet(ctx, "tls-auth-clients", "yes").Err())
-		require.NoError(t, c.Close())
-		c = srv.NewTLSClientWithOption(&redis.Options{TLSConfig: &tls.Config{InsecureSkipVerify: true}})
-		require.Error(t, c.Ping(ctx).Err())
-	})
+			require.NoError(t, rdb.ConfigSet(ctx, "tls-auth-clients", "yes").Err())
+			require.NoError(t, c.Close())
+			c = srv.NewTLSClientWithOption(&redis.Options{TLSConfig: &tls.Config{InsecureSkipVerify: true}})
+			require.Error(t, c.Ping(ctx).Err())
+		})
+	}
 }

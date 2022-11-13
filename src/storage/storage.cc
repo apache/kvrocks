5d74c115b160b47d7a681c975a362f2675763f33
dl239 <dl239@126.com>
2022-11-13 09:43:18 +0800

    fix: modernize-make-unique

--- src/storage/storage.cc
+++ src/storage/storage.cc
@@ -946,8 +946,7 @@ bool Storage::ReplDataManager::FileExists(Storage *storage, const std::string &d
   uint64_t size;
   s = storage->env_->GetFileSize(file_path, &size);
   if (!s.ok()) return false;
-  std::unique_ptr<rocksdb::SequentialFileWrapper> src_reader;
-  src_reader.reset(new rocksdb::SequentialFileWrapper(src_file.get()));
+  auto src_reader = std::make_unique<rocksdb::SequentialFileWrapper>(rocksdb::SequentialFileWrapper(src_file.get()));
 
   char buffer[4096];
   Slice slice;

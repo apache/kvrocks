95d290e8b13be50f8faf8b7bd0cb7d1e47aeecb5
maochongxin <maochongxin@gmail.com>
2022-11-15 16:20:52 +0800

    Fix cppcoreguidelines-special-member-functions warning reported by clang-tidy

--- src/commands/redis_cmd.cc
+++ src/commands/redis_cmd.cc
@@ -1661,6 +1661,15 @@ class CommandRPop : public CommandPop {
 class CommandBPop : public Commander {
  public:
   explicit CommandBPop(bool left) : left_(left) {}
+
+  CommandBPop() = default;
+
+  // Not copyable
+  CommandBPop(const CommandBPop&) = delete;
+  CommandBPop(CommandBPop&&) = delete;
+  CommandBPop& operator=(const CommandBPop&)  = delete;
+  CommandBPop& operator=(CommandBPop&&)  = delete;
+
   ~CommandBPop() override {
     if (timer_ != nullptr) {
       event_free(timer_);

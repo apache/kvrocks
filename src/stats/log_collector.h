a787beab79cb4879131dc2c2c3a453b6c7aaefdf
HaveAnOrangeCat <maochongxin@gmail.com>
2022-11-15 23:40:04 +0800

    Fix cppcoreguidelines-special-member-functions warning reported by cl… (#1124)
    Co-authored-by: Twice <twice@apache.org>
    Co-authored-by: Twice <twice.mliu@gmail.com>

--- src/stats/log_collector.h
+++ src/stats/log_collector.h
@@ -56,6 +56,9 @@ class PerfEntry {
 template <class T>
 class LogCollector {
  public:
+  LogCollector() = default;
+  LogCollector(const LogCollector &) = delete;
+  LogCollector &operator=(const LogCollector &) = delete;
   ~LogCollector();
   ssize_t Size();
   void Reset();

96706d829ac693c47863d825a4493100f3126c2d
PragmaTwice <twice.mliu@gmail.com>
2023-02-20 16:46:20 +0800

    Remove some new-delete patterns and refine LockManager hash function

--- tests/cppunit/log_collector_test.cc
+++ tests/cppunit/log_collector_test.cc
@@ -25,12 +25,12 @@
 TEST(LogCollector, PushEntry) {
   LogCollector<PerfEntry> perf_log;
   perf_log.SetMaxEntries(1);
-  perf_log.PushEntry(new PerfEntry());
-  perf_log.PushEntry(new PerfEntry());
+  perf_log.PushEntry(std::make_unique<PerfEntry>());
+  perf_log.PushEntry(std::make_unique<PerfEntry>());
   EXPECT_EQ(perf_log.Size(), 1);
   perf_log.SetMaxEntries(2);
-  perf_log.PushEntry(new PerfEntry());
-  perf_log.PushEntry(new PerfEntry());
+  perf_log.PushEntry(std::make_unique<PerfEntry>());
+  perf_log.PushEntry(std::make_unique<PerfEntry>());
   EXPECT_EQ(perf_log.Size(), 2);
   perf_log.Reset();
   EXPECT_EQ(perf_log.Size(), 0);

523b70ef67454bee15d40c9689f31bea91bfd7f9
tison <wander4096@gmail.com>
2023-04-05 15:28:05 +0800

    chore: tidy the NOTICE file (#1370)
    Signed-off-by: tison <wander4096@gmail.com>

--- src/common/rand.cc
+++ src/common/rand.cc
@@ -1,28 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
- */
-
-/*
- * Rewritten in C++ with constexprs and templates by @tanruixiang in
- * https://github.com/apache/incubator-kvrocks/commit/900e0f44781d9459649e9864ba09abe6000ce987
- */
-
 /* Pseudo random number generation functions derived from the drand48()
  * function obtained from pysam source code.
  *
@@ -64,6 +39,11 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Rewritten in C++ with constexprs and templates by @tanruixiang in
+ * https://github.com/apache/incubator-kvrocks/commit/900e0f44781d9459649e9864ba09abe6000ce987
  */
 
 #include "rand.h"

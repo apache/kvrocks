d9acccebd9e2cd225603f8f95ac5dc9fd2376bc9
Twice <twice.mliu@gmail.com>
2023-05-11 19:43:46 +0800

    Rename KvrocksLabs to RocksLabs in cmake (#1436)

--- cmake/luajit.cmake
+++ cmake/luajit.cmake
@@ -40,7 +40,7 @@ if ((${CMAKE_SYSTEM_NAME} MATCHES "Darwin") AND (NOT CMAKE_OSX_DEPLOYMENT_TARGET
 endif ()
 
 FetchContent_DeclareGitHubWithMirror(luajit
-        KvrocksLabs/LuaJIT c0a8e68325ec261a77bde1c8eabad398168ffe74
+        RocksLabs/LuaJIT c0a8e68325ec261a77bde1c8eabad398168ffe74
         MD5=7ff3e5ca4ddec59be2c2f97c5ff881d0)
 
 FetchContent_GetProperties(luajit)

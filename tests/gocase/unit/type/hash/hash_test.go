75310189d32097d54363a3b937f8350af847eca7
Zewen Xu <zevin9427@gmail.com>
2023-05-23 23:42:48 +0800

    test: Improve HMGET test coverage
    - Add test for HMGET with missing middle field
    
    Signed-off-by: Zewen Xu <zevin9427@gmail.com>

--- tests/gocase/unit/type/hash/hash_test.go
+++ tests/gocase/unit/type/hash/hash_test.go
@@ -271,6 +271,14 @@ func TestHash(t *testing.T) {
 		pattern := ".*wrong.*"
 		util.ErrorRegexp(t, rdb.HMGet(ctx, "wrongtype", "field1", "field2").Err(), pattern)
 	})
+	
+	t.Run("HMGET succeed field return with missing middle field", func(t *testing.T) {
+		require.Equal(t, int64(2), rdb.HSet(ctx, "successreturn", "name", "1", "age", "2").Val())
+		cmd := rdb.HMGet(ctx, "successreturn", "name", "a", "age", "name")
+		actual, _ := cmd.Result()
+		expected := []interface{}{"1", nil, "2", "1"}
+		require.Equal(t, expected, actual)
+	})
 
 	t.Run("HMGET - small hash", func(t *testing.T) {
 		var keys []string

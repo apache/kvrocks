1239dba421e47bc263e987b5dbc58df7cff033f5
hulk <hulk.website@gmail.com>
2023-06-26 14:16:45 +0800

    chore: Drop incubator- prefix from Go test files, as well as yaml files and comment lines (#1519)

--- tests/gocase/unit/type/zset/zset_test.go
+++ tests/gocase/unit/type/zset/zset_test.go
@@ -30,7 +30,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/apache/incubator-kvrocks/tests/gocase/util"
+	"github.com/apache/kvrocks/tests/gocase/util"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/require"
 	"golang.org/x/exp/slices"

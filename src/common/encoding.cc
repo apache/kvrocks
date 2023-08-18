1a52f466017dfd5c324de0e4e8035103fe56fc69
PragmaTwice <twice@apache.org>
2023-08-18 22:05:07 +0800

    Refactor encoding via templates

--- src/common/encoding.cc
+++ src/common/encoding.cc
@@ -29,150 +29,46 @@
 #include <string>
 #include <utility>
 
-void EncodeFixed8(char *buf, uint8_t value) { buf[0] = static_cast<char>(value & 0xff); }
+inline uint64_t EncodeDoubleToUInt64(double value) {
+  uint64_t result = 0;
 
-void EncodeFixed16(char *buf, uint16_t value) {
-  if constexpr (IsLittleEndian()) {
-    value = __builtin_bswap16(value);
-  }
-  __builtin_memcpy(buf, &value, sizeof(value));
-}
+  __builtin_memcpy(&result, &value, sizeof(value));
 
-void EncodeFixed32(char *buf, uint32_t value) {
-  if constexpr (IsLittleEndian()) {
-    value = __builtin_bswap32(value);
-  }
-  __builtin_memcpy(buf, &value, sizeof(value));
-}
-
-void EncodeFixed64(char *buf, uint64_t value) {
-  if constexpr (IsLittleEndian()) {
-    value = __builtin_bswap64(value);
-  }
-  __builtin_memcpy(buf, &value, sizeof(value));
-}
-
-void PutFixed8(std::string *dst, uint8_t value) {
-  char buf[1];
-  EncodeFixed8(buf, value);
-  dst->append(buf, sizeof(buf));
-}
-
-void PutFixed16(std::string *dst, uint16_t value) {
-  char buf[sizeof(value)];
-  EncodeFixed16(buf, value);
-  dst->append(buf, sizeof(buf));
-}
-
-void PutFixed32(std::string *dst, uint32_t value) {
-  char buf[sizeof(value)];
-  EncodeFixed32(buf, value);
-  dst->append(buf, sizeof(buf));
-}
-
-void PutFixed64(std::string *dst, uint64_t value) {
-  char buf[sizeof(value)];
-  EncodeFixed64(buf, value);
-  dst->append(buf, sizeof(buf));
-}
-
-void PutDouble(std::string *dst, double value) {
-  uint64_t u64 = 0;
-
-  __builtin_memcpy(&u64, &value, sizeof(value));
-
-  if ((u64 >> 63) == 1) {
+  if ((result >> 63) == 1) {
     // signed bit would be zero
-    u64 ^= 0xffffffffffffffff;
+    result ^= 0xffffffffffffffff;
   } else {
     // signed bit would be one
-    u64 |= 0x8000000000000000;
+    result |= 0x8000000000000000;
   }
 
-  PutFixed64(dst, u64);
+  return result;
 }
 
-bool GetFixed8(rocksdb::Slice *input, uint8_t *value) {
-  const char *data = nullptr;
-  if (input->size() < sizeof(uint8_t)) {
-    return false;
+inline double DecodeDoubleFromUInt64(uint64_t value) {
+  if ((value >> 63) == 0) {
+    value ^= 0xffffffffffffffff;
+  } else {
+    value &= 0x7fffffffffffffff;
   }
-  data = input->data();
-  *value = static_cast<uint8_t>(data[0] & 0xff);
-  input->remove_prefix(sizeof(uint8_t));
-  return true;
+
+  double result = 0;
+  __builtin_memcpy(&result, &value, sizeof(result));
+
+  return result;
 }
 
-bool GetFixed64(rocksdb::Slice *input, uint64_t *value) {
-  if (input->size() < sizeof(uint64_t)) {
-    return false;
-  }
-  *value = DecodeFixed64(input->data());
-  input->remove_prefix(sizeof(uint64_t));
-  return true;
-}
+void EncodeDouble(char *buf, double value) { EncodeFixed64(buf, EncodeDoubleToUInt64(value)); }
 
-bool GetFixed32(rocksdb::Slice *input, uint32_t *value) {
-  if (input->size() < sizeof(uint32_t)) {
-    return false;
-  }
-  *value = DecodeFixed32(input->data());
-  input->remove_prefix(sizeof(uint32_t));
-  return true;
-}
+void PutDouble(std::string *dst, double value) { PutFixed64(dst, EncodeDoubleToUInt64(value)); }
 
-bool GetFixed16(rocksdb::Slice *input, uint16_t *value) {
-  if (input->size() < sizeof(uint16_t)) {
-    return false;
-  }
-  *value = DecodeFixed16(input->data());
-  input->remove_prefix(sizeof(uint16_t));
-  return true;
-}
+double DecodeDouble(const char *ptr) { return DecodeDoubleFromUInt64(DecodeFixed64(ptr)); }
 
 bool GetDouble(rocksdb::Slice *input, double *value) {
   if (input->size() < sizeof(double)) return false;
   *value = DecodeDouble(input->data());
   input->remove_prefix(sizeof(double));
   return true;
-}
-
-uint16_t DecodeFixed16(const char *ptr) {
-  uint16_t value = 0;
-
-  __builtin_memcpy(&value, ptr, sizeof(value));
-
-  return IsLittleEndian() ? __builtin_bswap16(value) : value;
-}
-
-uint32_t DecodeFixed32(const char *ptr) {
-  uint32_t value = 0;
-
-  __builtin_memcpy(&value, ptr, sizeof(value));
-
-  return IsLittleEndian() ? __builtin_bswap32(value) : value;
-}
-
-uint64_t DecodeFixed64(const char *ptr) {
-  uint64_t value = 0;
-
-  __builtin_memcpy(&value, ptr, sizeof(value));
-
-  return IsLittleEndian() ? __builtin_bswap64(value) : value;
-}
-
-double DecodeDouble(const char *ptr) {
-  uint64_t decoded = DecodeFixed64(ptr);
-
-  if ((decoded >> 63) == 0) {
-    decoded ^= 0xffffffffffffffff;
-  } else {
-    decoded &= 0x7fffffffffffffff;
-  }
-
-  double value = 0;
-  __builtin_memcpy(&value, &decoded, sizeof(value));
-  return value;
 }
 
 char *EncodeVarint32(char *dst, uint32_t v) {

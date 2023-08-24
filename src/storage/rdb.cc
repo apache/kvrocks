3a0e3009711557113c3b17d4d7881bab625755fb
hulk <hulk.website@gmail.com>
2023-08-24 23:12:36 +0800

    Update src/storage/rdb.cc
    Co-authored-by: Twice <twice@apache.org>

--- src/storage/rdb.cc
+++ src/storage/rdb.cc
@@ -119,12 +119,12 @@ StatusOr<std::string> RDB::loadLzfString() {
   auto len = GET_OR_RET(loadObjectLen(nullptr));
   GET_OR_RET(peekOk(static_cast<size_t>(compression_len)));
 
-  auto out_buf = make_unique<char[]>(len);
-  if (lzf_decompress(input_.data() + pos_, compression_len, out_buf.get(), len) != len) {
+  std::string out_buf(len, 0);
+  if (lzf_decompress(input_.data() + pos_, compression_len, out_buf.data(), len) != len) {
     return {Status::NotOK, "Invalid LZF compressed string"};
   }
   pos_ += compression_len;
-  return std::string().assign(out_buf.get(), len);
+  return out_buf;
 }
 
 StatusOr<std::string> RDB::loadEncodedString() {

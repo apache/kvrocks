c46a0e011da6e8509e82fc3f3df54a516a2f5676
git-hulk <hulk.website@gmail.com>
2023-09-23 23:21:57 +0800

    Allow to enable the replication of namespaces between master and slaves

---
+++ src/server/namespace.cc
@@ -0,0 +1,5 @@
+//
+// Created by hulk on 2023/9/22.
+//
+
+#include "namespace.h"
---
+++ src/server/namespace.h
@@ -0,0 +1,16 @@
+//
+// Created by hulk on 2023/9/22.
+//
+
+#ifndef KVROCKS_SRC_SERVER_NAMESPACE_H_
+#define KVROCKS_SRC_SERVER_NAMESPACE_H_
+
+
+
+class namespace {
+
+};
+
+
+
+#endif //KVROCKS_SRC_SERVER_NAMESPACE_H_

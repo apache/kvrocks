3c9b688300be80ee4ece4e115e751dc64239498c
kay011 <18236554121@163.com>
2024-02-07 18:25:52 +0800

    Support BIT and BYTE options in the BITCOUNT command (#2087)
    Co-authored-by: mwish <maplewish117@gmail.com>
    Co-authored-by: hulk <hulk.website@gmail.com>
    Co-authored-by: 纪华裕 <jihuayu123@gmail.com>

--- src/commands/cmd_bit.cc
+++ src/commands/cmd_bit.cc
@@ -120,8 +120,9 @@ class CommandBitCount : public Commander {
 
     if (args.size() == 5) {
       if (util::EqualICase(args[4], "BYTE")) {
+        is_bit_index_ = false;
       } else if (util::EqualICase(args[4], "BIT")) {
-        return {Status::RedisExecErr, errNotImplemented};
+        is_bit_index_ = true;
       } else {
         return {Status::RedisParseErr, errInvalidSyntax};
       }
@@ -133,7 +134,7 @@ class CommandBitCount : public Commander {
   Status Execute(Server *srv, Connection *conn, std::string *output) override {
     uint32_t cnt = 0;
     redis::Bitmap bitmap_db(srv->storage, conn->GetNamespace());
-    auto s = bitmap_db.BitCount(args_[1], start_, stop_, &cnt);
+    auto s = bitmap_db.BitCount(args_[1], start_, stop_, is_bit_index_, &cnt);
     if (!s.ok()) return {Status::RedisExecErr, s.ToString()};
 
     *output = redis::Integer(cnt);
@@ -143,6 +144,7 @@ class CommandBitCount : public Commander {
  private:
   int64_t start_ = 0;
   int64_t stop_ = -1;
+  bool is_bit_index_ = false;
 };
 
 class CommandBitPos : public Commander {

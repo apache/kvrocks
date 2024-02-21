f05d88594f7d24f2c641dfce406ca0a01d71f4f2
PragmaTwice <twice@apache.org>
2024-02-21 23:46:10 +0900

    Initialize the unified index updater for HASH and JSON data type

---
+++ src/search/indexer.h
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+#pragma once
+
+#include <tsl/htrie_map.h>
+
+#include <deque>
+#include <map>
+#include <utility>
+#include <variant>
+
+#include "commands/commander.h"
+#include "config/config.h"
+#include "indexer.h"
+#include "search/search_encoding.h"
+#include "server/server.h"
+#include "storage/redis_metadata.h"
+#include "types/redis_hash.h"
+#include "types/redis_json.h"
+
+namespace redis {
+
+struct GlobalIndexer;
+
+struct FieldValueRetriever {
+  struct HashInterm {
+    Hash hash;
+    HashMetadata metadata;
+    std::string_view key;
+
+    HashInterm(Hash hash, HashMetadata metadata, std::string_view key)
+        : hash(std::move(hash)), metadata(std::move(metadata)), key(key) {}
+  };
+  using JsonInterm = JsonValue;
+
+  using Variant = std::variant<HashInterm, JsonInterm>;
+  Variant db;
+
+  static StatusOr<FieldValueRetriever> Create(SearchOnDataType type, std::string_view key, engine::Storage *storage,
+                                              const std::string &ns);
+
+  explicit FieldValueRetriever(Hash hash, HashMetadata metadata, std::string_view key)
+      : db(std::in_place_type<HashInterm>, std::move(hash), std::move(metadata), key) {}
+
+  explicit FieldValueRetriever(JsonValue json) : db(std::in_place_type<JsonInterm>, std::move(json)) {}
+
+  rocksdb::Status Retrieve(std::string_view field, std::string *output);
+};
+
+struct IndexUpdater {
+  using FieldValues = std::map<std::string, std::string>;
+
+  SearchOnDataType on_data_type;
+  std::map<std::string, std::unique_ptr<SearchFieldMetadata>> fields;
+  GlobalIndexer *indexer = nullptr;
+
+  StatusOr<FieldValues> Record(std::string_view key, Connection *conn);
+};
+
+struct GlobalIndexer {
+  std::deque<IndexUpdater> updaters;
+  tsl::htrie_map<char, IndexUpdater *> prefix_map;
+
+  engine::Storage *storage = nullptr;
+
+  StatusOr<IndexUpdater::FieldValues> Record(std::string_view key, Connection *conn);
+};
+
+}  // namespace redis

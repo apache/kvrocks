7c1472a177f8d7e7f02ee4080c629734858584b2
PragmaTwice <twice@apache.org>
2024-03-28 03:14:03 +0900

    add test

--- tests/cppunit/ir_dot_dumper_test.cc
+++ tests/cppunit/ir_dot_dumper_test.cc
@@ -20,6 +20,9 @@
 
 #include "search/ir_dot_dumper.h"
 
+#include <regex>
+#include <sstream>
+
 #include "gtest/gtest.h"
 #include "search/sql_transformer.h"
 
@@ -30,6 +33,23 @@ static auto Parse(const std::string& in) { return sql::ParseToIR(peg::string_inp
 TEST(DotDumperTest, Simple) {
   auto ir = *Parse("select a from b where c = 1 or d hastag \"x\" and 2 <= e order by e asc limit 0, 10");
 
-  DotDumper dumper{std::cout};
+  std::stringstream ss;
+  DotDumper dumper{ss};
+
   dumper.Dump(ir.get());
+
+  std::string dot = ss.str();
+  std::smatch matches;
+
+  std::regex_search(dot, matches, std::regex(R"((\w+) \[ label = "SearchStmt)"));
+  auto search_stmt = matches[1].str();
+
+  std::regex_search(dot, matches, std::regex(R"((\w+) \[ label = "OrExpr)"));
+  auto or_expr = matches[1].str();
+
+  std::regex_search(dot, matches, std::regex(R"((\w+) \[ label = "AndExpr)"));
+  auto and_expr = matches[1].str();
+
+  ASSERT_NE(dot.find(fmt::format("{} -> {}", search_stmt, or_expr)), std::string::npos);
+  ASSERT_NE(dot.find(fmt::format("{} -> {}", or_expr, and_expr)), std::string::npos);
 }

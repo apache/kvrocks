4513e4f54a3c5ae2a297d666ea00e1eb0f5ff0bb
80597928 <673421862@qq.com>
2024-04-19 15:36:28 +0800

    fix

--- tests/gocase/unit/scan/scan_test.go
+++ tests/gocase/unit/scan/scan_test.go
@@ -310,9 +310,9 @@ func ScanTest(t *testing.T, rdb *redis.Client, ctx context.Context) {
 	        require.Equal(t, []string{"stringtype1","stringtype2","stringtype3"}, scanAll(t, rdb, "match", "key:*","type","string"))
             require.Equal(t, []string{"stringtype1","stringtype2","stringtype3"}, scanAll(t, rdb, "match", "key:*","count","3","type","string"))
 	        //hash type
-	        require.NoError(t, rdb.HSet(ctx, "hashtype1", "key1", "val1", "key2", "val2").Val())
-	        require.NoError(t, rdb.HSet(ctx, "hashtype2", "key1", "val1", "key2", "val2").Val())
-	        require.NoError(t, rdb.HSet(ctx, "hashtype3", "key1", "val1", "key2", "val2").Val())
+	        require.NoError(t, rdb.HSet(ctx, "hashtype1", "key1", "val1", "key2", "val2").Err())
+	        require.NoError(t, rdb.HSet(ctx, "hashtype2", "key1", "val1", "key2", "val2").Err())
+	        require.NoError(t, rdb.HSet(ctx, "hashtype3", "key1", "val1", "key2", "val2").Err())
 	        require.Equal(t, []string{"hashtype1","hashtype2","hashtype3"}, scanAll(t, rdb, "match", "key:*","type","hash"))
 	       	require.Equal(t, []string{"hashtype1","hashtype2","hashtype3"}, scanAll(t, rdb, "match", "key:*","count","3","type","hash"))
             //list type
@@ -322,6 +322,7 @@ func ScanTest(t *testing.T, rdb *redis.Client, ctx context.Context) {
 	        require.Equal(t, []string{"listtype1","listtype2","listtype3"}, scanAll(t, rdb, "match", "key:*","type","list"))
             require.Equal(t, []string{"listtype1","listtype2","listtype3"}, scanAll(t, rdb, "match", "key:*","count","3","type","list"))
             //set type
+            var elements []redis.Z
             for i := 0; i < 10; i++ {
 				elements = append(elements, test.keyGen(i))
 			}
@@ -349,8 +350,23 @@ func ScanTest(t *testing.T, rdb *redis.Client, ctx context.Context) {
   	       require.Equal(t, []string{"bitmaptype1","bitmaptype2","bitmaptype3"}, scanAll(t, rdb, "match", "key:*","type","bitmap"))
   	       require.Equal(t, []string{"bitmaptype1","bitmaptype2","bitmaptype3"}, scanAll(t, rdb, "match", "key:*","count","3","type","bitmap"))
            //stream type
-
-
+           require.NoError(t, rdb.XAdd(ctx, &redis.XAddArgs{Stream: "streamtype1", Values: []string{"item", "1", "value", "a"}}).Err())
+		   require.NoError(t, rdb.XAdd(ctx, &redis.XAddArgs{Stream: "streamtype2", Values: []string{"item", "1", "value", "a"}}).Err())
+		   require.NoError(t, rdb.XAdd(ctx, &redis.XAddArgs{Stream: "streamtype3", Values: []string{"item", "1", "value", "a"}}).Err())
+  	       require.Equal(t, []string{"streamtype1","streamtype2","streamtype3"}, scanAll(t, rdb, "match", "key:*","type","stream"))
+  	       require.Equal(t, []string{"streamtype1","streamtype2","streamtype3"}, scanAll(t, rdb, "match", "key:*","type","count","3","stream"))
+           //MBbloom type
+           require.NoError(t, rdb.Do(ctx, "bf.reserve", "MBbloomtype1", "0.02", "1000").Err())
+           require.NoError(t, rdb.Do(ctx, "bf.reserve", "MBbloomtype2", "0.02", "1000").Err())
+           require.NoError(t, rdb.Do(ctx, "bf.reserve", "MBbloomtype3", "0.02", "1000").Err())
+  	       require.Equal(t, []string{"MBbloomtype1","MBbloomtype2","MBbloomtype3"}, scanAll(t, rdb, "match", "key:*","type","MBbloom--"))
+  	       require.Equal(t, []string{"MBbloomtype1","MBbloomtype2","MBbloomtype3"}, scanAll(t, rdb, "match", "key:*","type","count","3","MBbloom--"))
+           //ReJSON-RL
+           require.NoError(t, rdb.Do(ctx, "JSON.SET", "ReJSONtype1", "$", ` {"x":1, "y":2} `).Err())
+		   require.NoError(t, rdb.Do(ctx, "JSON.SET", "ReJSONtype2", "$", ` {"x":1, "y":2} `).Err())
+		   require.NoError(t, rdb.Do(ctx, "JSON.SET", "ReJSONtype3", "$", ` {"x":1, "y":2} `).Err())
+  	       require.Equal(t, []string{"ReJSONtype1","ReJSONtype2","ReJSONtype3"}, scanAll(t, rdb, "match", "key:*","type","ReJSON-RL"))
+  	       require.Equal(t, []string{"ReJSONtype1","ReJSONtype2","ReJSONtype3"}, scanAll(t, rdb, "match", "key:*","count","3","type","ReJSON-RL"))
     	})
 
 

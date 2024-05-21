e45dcd198db16d6f74e82b017cd147296d3806ca
80597928 <673421862@qq.com>
2024-05-21 18:43:47 +0800

    fix

--- tests/gocase/unit/type/json/json_test.go
+++ tests/gocase/unit/type/json/json_test.go
@@ -630,11 +630,11 @@ func TestJson(t *testing.T) {
 
 	t.Run("JSON.DEBUG MEMORY basics", func(t *testing.T) {
 		var result1 = make([]interface{}, 0)
-		result1 = append(result1, int64(5))
+		result1 = append(result1, int64(4))
 		require.NoError(t, rdb.Do(ctx, "JSON.SET", "a", "$", `{"a":"foo", "nested": {"a": "hello"}, "nested2": {"a": 31}}`).Err())
 		require.Equal(t, rdb.Do(ctx, "JSON.DEBUG", "MEMORY", "a", "$.a").Val(), result1)
 		var result2 = make([]interface{}, 0)
-		result2 = append(result2, int64(5), int64(7), int64(2))
+		result2 = append(result2, int64(4), int64(7), int64(2))
 		require.Equal(t, rdb.Do(ctx, "JSON.DEBUG", "MEMORY", "a", "$..a").Val(), result2)
 		require.ErrorIs(t, rdb.Do(ctx, "JSON.DEBUG", "MEMORY", "not_exists", "$").Err(), redis.Nil)
 	})

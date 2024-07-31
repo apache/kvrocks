727bec2e69958a170fc94c4ffec77e07ced25fce
tutububug <40481744+tutububug@users.noreply.github.com>
2024-07-31 13:34:23 +0800

    feat(hyperloglog): add support of the Hyperloglog data structure (#2142)
    Co-authored-by: yangxiao <yangxiao@shopee.com>
    Co-authored-by: hulk <hulk.website@gmail.com>
    Co-authored-by: mwish <maplewish117@gmail.com>

---
+++ src/types/redis_hyperloglog.h
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+#pragma once
+
+#include "storage/redis_db.h"
+#include "storage/redis_metadata.h"
+
+namespace redis {
+
+class HyperLogLog : public Database {
+ public:
+  explicit HyperLogLog(engine::Storage *storage, const std::string &ns) : Database(storage, ns) {}
+  rocksdb::Status Add(const Slice &user_key, const std::vector<uint64_t> &element_hashes, uint64_t *ret);
+  rocksdb::Status Count(const Slice &user_key, uint64_t *ret);
+  // TODO(mwish): Supports merge operation and related commands
+  // rocksdb::Status Merge(const std::vector<Slice> &user_keys);
+
+  static uint64_t HllHash(std::string_view);
+
+ private:
+  rocksdb::Status GetMetadata(Database::GetOptions get_options, const Slice &ns_key, HyperLogLogMetadata *metadata);
+  /// Using multi-get to acquire the register_segments
+  rocksdb::Status getRegisters(Database::GetOptions get_options, const Slice &ns_key,
+                               std::vector<rocksdb::PinnableSlice> *register_segments);
+};
+
+}  // namespace redis
---
+++ tests/cppunit/types/hyperloglog_test.cc
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+#include "test_base.h"
+#include "types/redis_hyperloglog.h"
+
+class RedisHyperLogLogTest : public TestBase {
+ protected:
+  explicit RedisHyperLogLogTest() : TestBase() {
+    hll_ = std::make_unique<redis::HyperLogLog>(storage_.get(), "hll_ns");
+  }
+  ~RedisHyperLogLogTest() override = default;
+
+  std::unique_ptr<redis::HyperLogLog> hll_;
+
+  static std::vector<uint64_t> computeHashes(const std::vector<std::string_view> &elements) {
+    std::vector<uint64_t> hashes;
+    hashes.reserve(elements.size());
+    for (const auto &element : elements) {
+      hashes.push_back(redis::HyperLogLog::HllHash(element));
+    }
+    return hashes;
+  }
+};
+
+TEST_F(RedisHyperLogLogTest, PFADD) {
+  uint64_t ret = 0;
+  ASSERT_TRUE(hll_->Add("hll", {}, &ret).ok() && ret == 0);
+  // Approximated cardinality after creation is zero
+  ASSERT_TRUE(hll_->Count("hll", &ret).ok() && ret == 0);
+  // PFADD returns 1 when at least 1 reg was modified
+  ASSERT_TRUE(hll_->Add("hll", computeHashes({"a", "b", "c"}), &ret).ok());
+  ASSERT_EQ(1, ret);
+  ASSERT_TRUE(hll_->Count("hll", &ret).ok());
+  ASSERT_EQ(3, ret);
+  // PFADD returns 0 when no reg was modified
+  ASSERT_TRUE(hll_->Add("hll", computeHashes({"a", "b", "c"}), &ret).ok() && ret == 0);
+  // PFADD works with empty string
+  ASSERT_TRUE(hll_->Add("hll", computeHashes({""}), &ret).ok() && ret == 1);
+  // PFADD works with similar hash, which is likely to be in the same bucket
+  ASSERT_TRUE(hll_->Add("hll", {1, 2, 3, 2, 1}, &ret).ok() && ret == 1);
+  ASSERT_TRUE(hll_->Count("hll", &ret).ok());
+  ASSERT_EQ(7, ret);
+}
+
+TEST_F(RedisHyperLogLogTest, PFCOUNT_returns_approximated_cardinality_of_set) {
+  uint64_t ret = 0;
+  // pf add "1" to "5"
+  ASSERT_TRUE(hll_->Add("hll", computeHashes({"1", "2", "3", "4", "5"}), &ret).ok() && ret == 1);
+  // pf count is 5
+  ASSERT_TRUE(hll_->Count("hll", &ret).ok() && ret == 5);
+  // pf add "6" to "10"
+  ASSERT_TRUE(hll_->Add("hll", computeHashes({"6", "7", "8", "8", "9", "10"}), &ret).ok() && ret == 1);
+  // pf count is 10
+  ASSERT_TRUE(hll_->Count("hll", &ret).ok() && ret == 10);
+}

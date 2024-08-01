a3863f9cb8f84c15bec97a5382cd4e1bb76aa04e
mwish <maplewish117@gmail.com>
2024-08-01 20:58:11 +0800

    feat(hyperloglog): Add support of PFMERGE command (#2457)

--- src/types/redis_hyperloglog.h
+++ src/types/redis_hyperloglog.h
@@ -30,16 +30,26 @@ class HyperLogLog : public Database {
   explicit HyperLogLog(engine::Storage *storage, const std::string &ns) : Database(storage, ns) {}
   rocksdb::Status Add(const Slice &user_key, const std::vector<uint64_t> &element_hashes, uint64_t *ret);
   rocksdb::Status Count(const Slice &user_key, uint64_t *ret);
-  // TODO(mwish): Supports merge operation and related commands
-  // rocksdb::Status Merge(const std::vector<Slice> &user_keys);
+  /// The count when user_keys.size() is greater than 1.
+  rocksdb::Status CountMultiple(const std::vector<Slice> &user_key, uint64_t *ret);
+  rocksdb::Status Merge(const Slice &dest_user_key, const std::vector<Slice> &source_user_keys);
 
   static uint64_t HllHash(std::string_view);
 
  private:
-  rocksdb::Status GetMetadata(Database::GetOptions get_options, const Slice &ns_key, HyperLogLogMetadata *metadata);
+  [[nodiscard]] rocksdb::Status GetMetadata(Database::GetOptions get_options, const Slice &ns_key,
+                                            HyperLogLogMetadata *metadata);
+
+  [[nodiscard]] rocksdb::Status mergeUserKeys(Database::GetOptions get_options, const std::vector<Slice> &user_keys,
+                                              std::vector<std::string> *register_segments);
   /// Using multi-get to acquire the register_segments
-  rocksdb::Status getRegisters(Database::GetOptions get_options, const Slice &ns_key,
-                               std::vector<rocksdb::PinnableSlice> *register_segments);
+  ///
+  /// If the metadata is not found, register_segments will be initialized with 16 empty slices.
+  [[nodiscard]] rocksdb::Status getRegisters(Database::GetOptions get_options, const Slice &ns_key,
+                                             std::vector<rocksdb::PinnableSlice> *register_segments);
+  /// Same with getRegisters, but the result is stored in a vector of strings.
+  [[nodiscard]] rocksdb::Status getRegisters(Database::GetOptions get_options, const Slice &ns_key,
+                                             std::vector<std::string> *register_segments);
 };
 
 }  // namespace redis

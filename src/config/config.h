47bce819e44c5fc53052ebca0b933d5c6bec267f
SiLe Zhou <zhousile@foxmail.com>
2024-08-25 22:21:11 +0800

    Update src/config/config.h
    Co-authored-by: mwish <maplewish117@gmail.com>

--- src/config/config.h
+++ src/config/config.h
@@ -168,7 +168,7 @@ struct Config {
   int json_max_nesting_depth = 1024;
   JsonStorageFormat json_storage_format = JsonStorageFormat::JSON;
 
-  // transactional mode engine::Context
+  // Enable transactional mode in engine::Context
   bool txn_context_enabled = false;
 
   struct RocksDB {

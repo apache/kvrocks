6eb923af90810758efdad98e859248d736a6a54f
SiLe Zhou <zhousile@foxmail.com>
2024-10-11 11:09:01 +0800

    refactor: promote the `engine::Context` in the `Execute` (#2586)
    Co-authored-by: Twice <twice.mliu@gmail.com>

--- src/commands/cmd_txn.cc
+++ src/commands/cmd_txn.cc
@@ -29,7 +29,8 @@ namespace redis {
 
 class CommandMulti : public Commander {
  public:
-  Status Execute([[maybe_unused]] Server *srv, Connection *conn, std::string *output) override {
+  Status Execute([[maybe_unused]] engine::Context &ctx, [[maybe_unused]] Server *srv, Connection *conn,
+                 std::string *output) override {
     if (conn->IsFlagEnabled(Connection::kMultiExec)) {
       return {Status::RedisExecErr, "MULTI calls can not be nested"};
     }
@@ -43,7 +44,7 @@ class CommandMulti : public Commander {
 
 class CommandDiscard : public Commander {
  public:
-  Status Execute(Server *srv, Connection *conn, std::string *output) override {
+  Status Execute([[maybe_unused]] engine::Context &ctx, Server *srv, Connection *conn, std::string *output) override {
     if (!conn->IsFlagEnabled(Connection::kMultiExec)) {
       return {Status::RedisExecErr, "DISCARD without MULTI"};
     }
@@ -59,7 +60,7 @@ class CommandDiscard : public Commander {
 
 class CommandExec : public Commander {
  public:
-  Status Execute(Server *srv, Connection *conn, std::string *output) override {
+  Status Execute([[maybe_unused]] engine::Context &ctx, Server *srv, Connection *conn, std::string *output) override {
     if (!conn->IsFlagEnabled(Connection::kMultiExec)) {
       return {Status::RedisExecErr, "EXEC without MULTI"};
     }
@@ -92,7 +93,7 @@ class CommandExec : public Commander {
 
 class CommandWatch : public Commander {
  public:
-  Status Execute(Server *srv, Connection *conn, std::string *output) override {
+  Status Execute([[maybe_unused]] engine::Context &ctx, Server *srv, Connection *conn, std::string *output) override {
     if (conn->IsFlagEnabled(Connection::kMultiExec)) {
       return {Status::RedisExecErr, "WATCH inside MULTI is not allowed"};
     }
@@ -111,7 +112,7 @@ class CommandWatch : public Commander {
 
 class CommandUnwatch : public Commander {
  public:
-  Status Execute(Server *srv, Connection *conn, std::string *output) override {
+  Status Execute([[maybe_unused]] engine::Context &ctx, Server *srv, Connection *conn, std::string *output) override {
     srv->ResetWatchedKeys(conn);
     *output = redis::SimpleString("OK");
     return Status::OK();

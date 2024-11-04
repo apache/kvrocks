9d27d1a7cc48e0a367e265daaec773028f126c71
Aleks Lozovyuk <aleks.raiden@gmail.com>
2024-11-04 13:35:39 +0200

    ci: update crate-ci/typos to 1.27.0 (#2645)

--- src/types/redis_json.h
+++ src/types/redis_json.h
@@ -86,7 +86,7 @@ class Json : public Database {
  private:
   rocksdb::Status write(engine::Context &ctx, Slice ns_key, JsonMetadata *metadata, const JsonValue &json_val);
   rocksdb::Status read(engine::Context &ctx, const Slice &ns_key, JsonMetadata *metadata, JsonValue *value);
-  static rocksdb::Status parse(const JsonMetadata &metadata, const Slice &json_byt, JsonValue *value);
+  static rocksdb::Status parse(const JsonMetadata &metadata, const Slice &json_byte, JsonValue *value);
   rocksdb::Status create(engine::Context &ctx, const std::string &ns_key, JsonMetadata &metadata,
                          const std::string &value);
   rocksdb::Status del(engine::Context &ctx, const Slice &ns_key);

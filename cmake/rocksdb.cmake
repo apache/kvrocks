5882ab076905ef7f94f81f136584615a7af72a57
Aleks Lozovyuk <aleks.raiden@gmail.com>
2025-01-03 04:25:52 +0200

    chore: bump rocksdb to v9.10.0 (#2705)

--- cmake/rocksdb.cmake
+++ cmake/rocksdb.cmake
@@ -26,8 +26,8 @@ endif()
 include(cmake/utils.cmake)
 
 FetchContent_DeclareGitHubWithMirror(rocksdb
-  facebook/rocksdb v9.9.3
-  MD5=c93f55998866a1043bb62d218f79db43
+  facebook/rocksdb v9.10.0
+  MD5=ccc55dbd6f89d41c5bf2cb5a4c83a714
 )
 
 FetchContent_GetProperties(jemalloc)

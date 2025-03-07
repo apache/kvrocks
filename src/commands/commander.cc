8db4511240f1a8451f55f7ca46ae39de85916f89
Himanshu A Garode <himanshu2006garode@gmail.com>
2025-03-07 15:29:04 +0530

    Update commander.cc

--- src/commands/commander.cc
+++ src/commands/commander.cc
@@ -24,21 +24,26 @@
 
 namespace redis {
 
-RegisterToCommandTable::RegisterToCommandTable(CommandCategory category,
-                                               std::initializer_list<CommandAttributes> list) {
+RegisterToCommandTable::RegisterToCommandTable(
+    CommandCategory category, std::initializer_list<CommandAttributes> list) {
   for (auto attr : list) {
     attr.category = category;
     CommandTable::redis_command_table.emplace_back(attr);
-    CommandTable::original_commands[attr.name] = &CommandTable::redis_command_table.back();
-    CommandTable::commands[attr.name] = &CommandTable::redis_command_table.back();
+    CommandTable::original_commands[attr.name] =
+        &CommandTable::redis_command_table.back();
+    CommandTable::commands[attr.name] =
+        &CommandTable::redis_command_table.back();
   }
 }
 
 RegisterToCommandTable key_commands(
     CommandCategory::kKey,
     {
-        {"del", CommandAttributes::WriteKey(CommandFactory<CommandDel>(), -2, 1, -1, 1)},
-        {"delprefix", CommandAttributes::WriteKey(CommandFactory<CommandDelPrefix>(), 2, 1, 1, 1)}, // Added DELPREFIX command
+        {"del", CommandAttributes::WriteKey(CommandFactory<CommandDel>(), -2, 1,
+                                            -1, 1)},
+        {"delprefix",
+         CommandAttributes::WriteKey(CommandFactory<CommandDelPrefix>(), 2, 1,
+                                     1, 1)}, // Added DELPREFIX command
     });
 
 }
@@ -51,13 +56,15 @@ CommandMap *CommandTable::Get() { return &commands; }
 
 void CommandTable::Reset() { commands = original_commands; }
 
-std::string CommandTable::GetCommandInfo(const CommandAttributes *command_attributes) {
+std::string
+CommandTable::GetCommandInfo(const CommandAttributes *command_attributes) {
   std::string command, command_flags;
   command.append(redis::MultiLen(6));
   command.append(redis::BulkString(command_attributes->name));
   command.append(redis::Integer(command_attributes->arity));
   command_flags.append(redis::MultiLen(1));
-  command_flags.append(redis::BulkString(command_attributes->InitialFlags() & kCmdWrite ? "write" : "readonly"));
+  command_flags.append(redis::BulkString(
+      command_attributes->InitialFlags() & kCmdWrite ? "write" : "readonly"));
   command.append(command_flags);
   auto key_range = command_attributes->InitialKeyRange().ValueOr({0, 0, 0});
   command.append(redis::Integer(key_range.first_key));
@@ -75,7 +82,8 @@ void CommandTable::GetAllCommandsInfo(std::string *info) {
   }
 }
 
-void CommandTable::GetCommandsInfo(std::string *info, const std::vector<std::string> &cmd_names) {
+void CommandTable::GetCommandsInfo(std::string *info,
+                                   const std::vector<std::string> &cmd_names) {
   info->append(redis::MultiLen(cmd_names.size()));
   for (const auto &cmd_name : cmd_names) {
     auto cmd_iter = commands.find(util::ToLower(cmd_name));
@@ -89,8 +97,9 @@ void CommandTable::GetCommandsInfo(std::string *info, const std::vector<std::str
   }
 }
 
-StatusOr<std::vector<int>> CommandTable::GetKeysFromCommand(const CommandAttributes *attributes,
-                                                            const std::vector<std::string> &cmd_tokens) {
+StatusOr<std::vector<int>>
+CommandTable::GetKeysFromCommand(const CommandAttributes *attributes,
+                                 const std::vector<std::string> &cmd_tokens) {
   int argc = static_cast<int>(cmd_tokens.size());
 
   if (!attributes->CheckArity(argc)) {
@@ -99,7 +108,8 @@ StatusOr<std::vector<int>> CommandTable::GetKeysFromCommand(const CommandAttribu
 
   auto cmd = attributes->factory();
   if (auto s = cmd->Parse(cmd_tokens); !s) {
-    return {Status::NotOK, "Invalid syntax found in this command arguments: " + s.Msg()};
+    return {Status::NotOK,
+            "Invalid syntax found in this command arguments: " + s.Msg()};
   }
 
   Status status;
@@ -107,7 +117,8 @@ StatusOr<std::vector<int>> CommandTable::GetKeysFromCommand(const CommandAttribu
 
   attributes->ForEachKeyRange(
       [&](const std::vector<std::string> &, CommandKeyRange key_range) {
-        key_range.ForEachKeyIndex([&](int i) { key_indexes.push_back(i); }, cmd_tokens.size());
+        key_range.ForEachKeyIndex([&](int i) { key_indexes.push_back(i); },
+                                  cmd_tokens.size());
       },
       cmd_tokens,
       [&](const auto &) {
@@ -125,7 +136,8 @@ bool CommandTable::IsExists(const std::string &name) {
   return original_commands.find(util::ToLower(name)) != original_commands.end();
 }
 
-Status CommandTable::ParseSlotRanges(const std::string &slots_str, std::vector<SlotRange> &slots) {
+Status CommandTable::ParseSlotRanges(const std::string &slots_str,
+                                     std::vector<SlotRange> &slots) {
   if (slots_str.empty()) {
     return {Status::NotOK, "No slots to parse."};
   }
@@ -133,7 +145,9 @@ Status CommandTable::ParseSlotRanges(const std::string &slots_str, std::vector<S
   std::vector<std::string> slot_ranges = util::Split(slots_str, " ");
   if (slot_ranges.empty()) {
     return {Status::NotOK,
-            fmt::format("Invalid slots: `{}`. No slots to parse. Please use spaces to separate slots.", slots_str)};
+            fmt::format("Invalid slots: `{}`. No slots to parse. Please use "
+                        "spaces to separate slots.",
+                        slots_str)};
   }
 
   auto valid_range = NumericRange<int>{0, kClusterSlots - 1};
@@ -151,21 +165,25 @@ Status CommandTable::ParseSlotRanges(const std::string &slots_str, std::vector<S
     // parse slot range: "int1-int2" (satisfy: int1 <= int2 )
     if (slot_range.front() == '-' || slot_range.back() == '-') {
       return {Status::NotOK,
-              fmt::format("Invalid slot range: `{}`. The character '-' can't appear in the first or last position.",
+              fmt::format("Invalid slot range: `{}`. The character '-' can't "
+                          "appear in the first or last position.",
                           slot_range)};
     }
     std::vector<std::string> fields = util::Split(slot_range, "-");
     if (fields.size() != 2) {
       return {Status::NotOK,
-              fmt::format("Invalid slot range: `{}`. The slot range should be of the form `int1-int2`.", slot_range)};
+              fmt::format("Invalid slot range: `{}`. The slot range should be "
+                          "of the form `int1-int2`.",
+                          slot_range)};
     }
     auto parse_start = ParseInt<int>(fields[0], valid_range, 10);
     auto parse_end = ParseInt<int>(fields[1], valid_range, 10);
     if (!parse_start || !parse_end || *parse_start > *parse_end) {
-      return {Status::NotOK,
-              fmt::format(
-                  "Invalid slot range: `{}`. The slot range `int1-int2` needs to satisfy the condition (int1 <= int2).",
-                  slot_range)};
+      return {
+          Status::NotOK,
+          fmt::format("Invalid slot range: `{}`. The slot range `int1-int2` "
+                      "needs to satisfy the condition (int1 <= int2).",
+                      slot_range)};
     }
     slots.emplace_back(*parse_start, *parse_end);
   }
@@ -173,4 +191,4 @@ Status CommandTable::ParseSlotRanges(const std::string &slots_str, std::vector<S
   return Status::OK();
 }
 
-}  // namespace redis
+} // namespace redis

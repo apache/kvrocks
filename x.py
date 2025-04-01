a6ab29d2facd43afec4fbaf6929f93149e4edbf9
ltagliamonte-dd <luigi.tagliamonte@doordash.com>
2025-04-01 16:02:44 -0700

    fix path

--- x.py
+++ x.py
@@ -337,7 +337,7 @@ def test_go(dir: str, cli_path: str, rest: List[str]) -> None:
     env = os.environ.copy()
     env.update({
         "CGO_CFLAGS": f"-I{rocksdb}",
-        "CGO_LDFLAGS": f"-L{rocksdb_lib} -L{zlib} -L{z4lib} -L{snappy_lib} -L/opt/rh/gcc-toolset-12/root/usr/lib64 -lstdc++",
+        "CGO_LDFLAGS": f"-L{rocksdb_lib} -L{zlib} -L{z4lib} -L{snappy_lib} -L/opt/rh/gcc-toolset-12/root/usr/lib/gcc/x86_64-redhat-linux/12/ -lstdc++",
     })
 
     args = [

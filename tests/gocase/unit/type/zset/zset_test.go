c006c939007fd86c256e59c33dd8216093853ea1
weimeng <weimeng@didiglobal.com>
2025-05-20 02:34:00 +0000

    fix(zset): wrong RESP reply in ZMSCORE command for non-existent key

--- tests/gocase/unit/type/zset/zset_test.go
+++ tests/gocase/unit/type/zset/zset_test.go
@@ -1309,6 +1309,9 @@ func basicTests(t *testing.T, rdb *redis.Client, ctx context.Context, enabledRES
 		res := rdb.ZMScore(ctx, "zset", "a", "b").Val()
 		require.Equal(t, int64(1), int64(res[0]))
 		require.Equal(t, int64(2), int64(res[1]))
+
+		require.Equal(t, redis.Nil, int64(rdb.ZMScore(ctx, "non-existent-zset", "a", "b").Val()[0]))
+		require.Equal(t, redis.Nil, int64(rdb.ZMScore(ctx, "non-existent-zset", "a", "b").Val()[1]))
 	})
 
 	t.Run(fmt.Sprintf("ZRANDMEMBER without scores - %s", encoding), func(t *testing.T) {

4d8eb6ba73bb3b71577587986772415a483fd063
sryanyuan <sryan@qq.com>
2025-06-27 14:02:51 +0800

    test(config): remove large value case

--- tests/gocase/unit/config/config_test.go
+++ tests/gocase/unit/config/config_test.go
@@ -388,7 +388,6 @@ func TestConfigSstFileDeleteRateBytesPerSec(t *testing.T) {
 		require.EqualValues(t, "0", result[parameter])
 
 		util.ErrorRegexp(t, cli.ConfigSet(ctx, parameter, "-1").Err(), ".*out of numeric range")
-		util.ErrorRegexp(t, cli.ConfigSet(ctx, parameter, "18446744073709551616").Err(), ".*Result too large")
 
 		require.NoError(t, cli.ConfigSet(ctx, parameter, "1073741824").Err())
 		result, err = cli.ConfigGet(ctx, parameter).Result()

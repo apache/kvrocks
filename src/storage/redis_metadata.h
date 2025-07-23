12ba46a1c209737ffe71febef34f4d0632adc4b0
DeEMO <yzzxrx@gmail.com>
2025-07-23 09:02:10 +0000

    Add default constructor for TimeSeriesMetadata

--- src/storage/redis_metadata.h
+++ src/storage/redis_metadata.h
@@ -388,8 +388,14 @@ class TimeSeriesMetadata : public Metadata {
   TSDuplicatePolicy duplicate_policy;
   std::string source_key;
 
-  explicit TimeSeriesMetadata(uint64_t retention_time, uint64_t chunk_size, ChunkType chunk_type,
-                              TSDuplicatePolicy duplicate_policy, bool generate_version = true)
+  explicit TimeSeriesMetadata(bool generate_version = true)
+      : Metadata(kRedisTimeSeries, generate_version),
+        retention_time(0),
+        chunk_size(1024),
+        chunk_type(ChunkType::UNCOMPRESSED),
+        duplicate_policy(TSDuplicatePolicy::BLOCK) {}
+  TimeSeriesMetadata(uint64_t retention_time, uint64_t chunk_size, ChunkType chunk_type,
+                     TSDuplicatePolicy duplicate_policy, bool generate_version = true)
       : Metadata(kRedisTimeSeries, generate_version),
         retention_time(retention_time),
         chunk_size(chunk_size),

3eb433cdd9e528dada30525df2dfbbcd91befc88
git-hulk <hulk.website@gmail.com>
2025-09-19 23:19:31 +0800

    Fix format

--- src/commands/cmd_script.cc
+++ src/commands/cmd_script.cc
@@ -134,8 +134,10 @@ uint64_t GenerateEvalFlags(uint64_t flags, const std::vector<std::string> &, con
 }
 
 REDIS_REGISTER_COMMANDS(
-    Script, MakeCmdAttr<CommandEval>("eval", -3, "write no-script skip-monitor", GetScriptEvalKeyRange, GenerateEvalFlags),
-    MakeCmdAttr<CommandEvalSHA>("evalsha", -3, "write no-script skip-monitor", GetScriptEvalKeyRange, GenerateEvalFlags),
+    Script,
+    MakeCmdAttr<CommandEval>("eval", -3, "write no-script skip-monitor", GetScriptEvalKeyRange, GenerateEvalFlags),
+    MakeCmdAttr<CommandEvalSHA>("evalsha", -3, "write no-script skip-monitor", GetScriptEvalKeyRange,
+                                GenerateEvalFlags),
     MakeCmdAttr<CommandEvalRO>("eval_ro", -3, "read-only no-script skip-monitor", GetScriptEvalKeyRange),
     MakeCmdAttr<CommandEvalSHARO>("evalsha_ro", -3, "read-only no-script skip-monitor", GetScriptEvalKeyRange),
     MakeCmdAttr<CommandScript>("script", -2, "exclusive no-script skip-monitor", NO_KEY, GenerateScriptFlags), )

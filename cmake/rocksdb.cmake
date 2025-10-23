363fccabc7f5bd18f2a37b119ca376493b1ad56d
Aleks Lozovyuk <aleks.raiden@gmail.com>
2025-10-23 09:43:54 +0300

    Update RocksDB version to v10.7.5

--- cmake/rocksdb.cmake
+++ cmake/rocksdb.cmake
@@ -26,8 +26,8 @@ endif()
 include(cmake/utils.cmake)
 
 FetchContent_DeclareGitHubWithMirror(rocksdb
-  facebook/rocksdb v10.4.2
-  MD5=2581c62b42cbb1c2d158bb2c54355b6e
+  facebook/rocksdb v10.7.5
+  MD5=9087131ad06ce6569fc02e6180ce11f9
 )
 
 FetchContent_GetProperties(jemalloc)

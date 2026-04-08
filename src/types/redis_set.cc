d52e83d03a5354a7e82c549d81cbe026eab2d319
songqing <zhangsongqing164@163.com>
2026-04-08 16:34:31 +0800

    fix comments

--- src/types/redis_set.cc
+++ src/types/redis_set.cc
@@ -115,7 +115,8 @@ rocksdb::Status Set::Remove(engine::Context &ctx, const Slice &user_key, const s
     }
     std::string sub_key = InternalKey(ns_key, member, metadata.version, storage_->IsSlotIdEncoded()).Encode();
     s = storage_->Get(ctx, ctx.GetReadOptions(), sub_key, &value);
-    if (!s.ok()) continue;
+    if (s.IsNotFound()) continue;
+    if (!s.ok()) return s;
     s = batch->Delete(sub_key);
     if (!s.ok()) return s;
     *removed_cnt += 1;

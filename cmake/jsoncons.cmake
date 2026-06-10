2f0ef5032834536aa1d7e4b190ea24a689d4922d
Aleks Lozovyuk <aleks.raiden@gmail.com>
2026-06-10 11:24:45 +0300

    Update jsoncons.cmake

--- cmake/jsoncons.cmake
+++ cmake/jsoncons.cmake
@@ -20,8 +20,8 @@ include_guard()
 include(cmake/utils.cmake)
 
 FetchContent_DeclareGitHubWithMirror(jsoncons
-  danielaparker/jsoncons v1.8.0
-  MD5=cade306909d7072d46c0a4dd872471f7
+  danielaparker/jsoncons v1.8.1
+  MD5=d8e11b4810115daf25dfea2db5c84f40
 )
 
 FetchContent_MakeAvailableWithArgs(jsoncons

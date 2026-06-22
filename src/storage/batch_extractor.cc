d07001097088c25c28a780fd586ef893fd7e9206
nagisa-kun <1434936049@qq.com>
2026-06-22 21:16:02 +0800

    revert blank

--- src/storage/batch_extractor.cc
+++ src/storage/batch_extractor.cc
@@ -261,7 +261,7 @@ rocksdb::Status WriteBatchExtractor::PutCF(uint32_t column_family_id, const Slic
         }
         break;
       }
-      // TODO: to implement the case of kRedisBloomFilter
+        // TODO: to implement the case of kRedisBloomFilter
       default:
         break;
     }
